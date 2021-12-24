b8c6a055b0988956b0607bb147a4374c2605ed47
mozilla2012 <earnold1993@gmail.com>
2021-12-24 10:12:10 -0800

    2021 02 Part 2

--- src/2021/02/Solution02.java
+++ src/2021/02/Solution02.java
@@ -30,6 +30,7 @@ class Solution02 {
         ArrayList<String> inputData = Utils.getInputStrings("src/2021/02");
         // System.out.println(inputData);
 
+        // Part 1
         int dep = 0;
         int hor = 0;
         for (String s : inputData) {
@@ -50,7 +51,30 @@ class Solution02 {
         }
         System.out.println(dep * hor);
 
-        
+        // Part 2
+        dep = 0;
+        hor = 0;
+        int aim = 0;
+        for (String s : inputData) {
+            String row[] = s.split(" ");
+            switch(row[0]) {
+                case "up":
+                    aim -= Integer.parseInt(row[1]);
+                    break;
+                case "down":
+                    aim += Integer.parseInt(row[1]);
+                    break;
+                case "forward":
+                    hor += Integer.parseInt(row[1]);
+                    dep += (Integer.parseInt(row[1]) * aim);
+                    break;
+                default:
+                    System.out.println("WTF");
+            }
+        }
+        System.out.println(dep * hor);
+
+
     }
 
 }
