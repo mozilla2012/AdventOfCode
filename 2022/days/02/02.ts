a8ce068d2b70fc559711d8d39c6d9c3f6b0a9694
mozilla2012 <earnold1993@gmail.com>
2022-12-04 22:46:21 -0800

    2022 02 Part 2

--- 2022/days/02/02.ts
+++ 2022/days/02/02.ts
@@ -5,7 +5,8 @@ export function adventMain(input: string): number {
 
     // The first column is what your opponent is going to play: 
     // A for Rock, B for Paper, and C for Scissors
-    // X for Rock, Y for Paper, and Z for Scissors
+    // Part 1: X for Rock, Y for Paper, and Z for Scissors
+    // Part 2: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
     const lines: string[] = input.split('\n');
     const scores: number[] = lines.map((line: string) => {
         const vals = line.split(' ');
@@ -14,13 +15,16 @@ export function adventMain(input: string): number {
             case 'A': { // Rock
                 switch(vals[1]) { 
                     case 'X': { 
-                        return 4; // 1 for Rock + 3 for draw
+                        // return 4; // 1 for Rock + 3 for draw
+                        return 3; // 0 for loss + 3 for scissors
                     } 
                     case 'Y': { 
-                        return 8; // 2 for paper + 6 for win
+                        // return 8; // 2 for paper + 6 for win
+                        return 4; // 3 for draw + 1 for rock
                     } 
                     case 'Z': { 
-                        return 3; // 3 for scissors + 0 for loss
+                        // return 3; // 3 for scissors + 0 for loss
+                        return 8; // 6 for win + 2 for paper
                     } 
                 }
                 break;
@@ -28,13 +32,16 @@ export function adventMain(input: string): number {
             case 'B': { // Paper
                 switch(vals[1]) { 
                     case 'X': { 
-                        return 1; // 1 for Rock + 0 for loss
+                        // return 1; // 1 for Rock + 0 for loss
+                        return 1; // 0 for loss + 1 for rock
                     } 
                     case 'Y': { 
-                        return 5; // 2 for paper +  3 for draw
+                        // return 5; // 2 for paper + 3 for draw
+                        return 5; // 3 for draw + 2 for paper
                     } 
                     case 'Z': { 
-                        return 9; // 3 for scissors + 6 for win
+                        // return 9; // 3 for scissors + 6 for win
+                        return 9; // 6 for win + 3 for scissors
                     } 
                 }
                 break;
@@ -42,17 +49,20 @@ export function adventMain(input: string): number {
             case 'C': { // Scissors
                 switch(vals[1]) { 
                     case 'X': { 
-                        return 7; // 1 for Rock + 6 for win
+                        // return 7; // 1 for Rock + 6 for win
+                        return 2; // 0 for loss + 2 for paper
                     } 
                     case 'Y': { 
-                        return 2; // 2 for paper + 0 for loss
+                        // return 2; // 2 for paper + 0 for loss
+                        return 6; // 3 for draw + 3 for scissors
                     } 
                     case 'Z': { 
-                        return 6; // 3 for scissors + 3 for draw
+                        // return 6; // 3 for scissors + 3 for draw
+                        return 7; // 6 for win + 1 for rock
                     } 
                 }
                 break;
-             } 
+            } 
         }
         throw new Error(`Illegal value entered: ${line}`);
     });
