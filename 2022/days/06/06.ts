f81666e918156136ab9382642fbe0c6bed76d21a
mozilla2012 <earnold1993@gmail.com>
2022-12-07 23:03:38 -0800

    2022 06 Part 2

--- 2022/days/06/06.ts
+++ 2022/days/06/06.ts
@@ -1,7 +1,7 @@
 // https://adventofcode.com/2022/day/6
 
 export function adventMain(input: string): any {
-    let sequence: string[] = input.substring(0, 4).split('');
+    let sequence: string[] = input.substring(0, 14).split('');
     for(let i = 4; i <= input.length; i++) {
         if (sequence.length === (new Set(sequence)).size) {
             return i;
