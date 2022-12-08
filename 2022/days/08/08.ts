bc20465ae0190a7656c3e8a5fe22665744efd32b
mozilla2012 <earnold1993@gmail.com>
2022-12-08 02:51:26 -0800

    2022 08 Part 2

--- 2022/days/08/08.ts
+++ 2022/days/08/08.ts
@@ -12,20 +12,26 @@ export function adventMain(input: string): any {
     const numRows = grid.length;
     
     let numVisibleTrees = (numCols * 2) + (numRows * 2) - 4; // Start by counting the border.
-    // console.log(numVisibleTrees);
 
+    let highScore = 0;
     // THE GRID; THE DIGITAL FRONTIER 
     for(let row = 1; row < numRows - 1; row++) {
         for(let col = 1; col < numCols - 1; col++) {
             if(visibleLeft(row, col, grid) || visibleRight(row, col, grid) || visibleTop(row, col, grid) || visibleBot(row, col, grid)){
                 numVisibleTrees++;
+                let score = scoreTree(row, col, grid);
+                if(score > highScore) {
+                    highScore = score;
+                }
             }
         }
     }
     
-    return numVisibleTrees;
+    // return numVisibleTrees; // Part 1
+    return highScore;  // Part 2
 }
 
+// All of this crap is highly repeated and could probably be golfed better, but now it's 3am and I don't want to right now :)
 function visibleLeft(row: number, col: number, grid: number[][]): boolean {
     const height: number = grid[row]![col]!;
     for(let i = 0; i < col; i++) {
@@ -64,4 +70,56 @@ function visibleBot(row: number, col: number, grid: number[][]): boolean {
         }
     }
     return true;
+}
+
+function scoreTree(row: number, col: number, grid: number[][]): number {
+    return scoreLeft(row, col, grid) * scoreRight(row, col, grid) * scoreTop(row, col, grid) * scoreBot(row, col, grid);
+}
+
+function scoreLeft(row: number, col: number, grid: number[][]): number {
+    let score = 0;
+    const height: number = grid[row]![col]!;
+    for(let i = col - 1; i >= 0; i--) {
+        score++;
+        if(grid[row]![i]! >= height) {
+            return score;
+        } 
+    }
+    return score;
+}
+
+function scoreRight(row: number, col: number, grid: number[][]): number {
+    let score = 0;
+    const height: number = grid[row]![col]!;
+    for(let i = col + 1; i < (grid[0]!.length); i++) {
+        score++;
+        if(grid[row]![i]! >= height) {
+            return score;
+        } 
+    }
+    return score;
+}
+
+function scoreTop(row: number, col: number, grid: number[][]): number {
+    let score = 0;
+    const height: number = grid[row]![col]!;
+    for(let i = row - 1; i >= 0; i--) {
+        score++;
+        if(grid[i]![col]! >= height) {
+            return score;
+        } 
+    }
+    return score;
+}
+
+function scoreBot(row: number, col: number, grid: number[][]): number {
+    let score = 0;
+    const height: number = grid[row]![col]!;
+    for(let i = row + 1; i < (grid.length); i++) {
+        score++;
+        if(grid[i]![col]! >= height) {
+            return score;
+        } 
+    }
+    return score;
 }
