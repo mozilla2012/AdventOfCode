1a4199981030b04f394d70f1c1cc6852ef3a6e2f
mozilla2012 <earnold1993@gmail.com>
2022-12-09 22:54:55 -0800

    Golfing 2022 10 Part 2

--- 2022/days/10/10.ts
+++ 2022/days/10/10.ts
@@ -22,33 +22,18 @@ export function adventMain(input: string): any {
             let val = parseInt(line.split(' ')[1]!);
             regX += val;
         }
+    }   
+    for(let row of screen) {
+        console.log(row.join(''));
     }
-    renderScreen();
     return sumStrength;
 }
 
 function incrementCycleAndCalcStrength(): void{
-    updateScreen();
+    screen[Math.floor((cycle-1) / 40)]![((cycle-1) % 40)] =  (regX === ((cycle-1) % 40) || (regX - 1) === ((cycle-1) % 40) || (regX + 1) === ((cycle-1) % 40)) ? 'X' : '.'; // Update the screen.
     if( ((cycle + 20) % 40) === 0 && cycle <= 220) {
         let strength = (regX * cycle);
         sumStrength += strength;
     }
     cycle++;
-}
-
-function updateScreen(): void {
-    let row = Math.floor((cycle-1) / 40);
-    let col = (cycle-1) % 40;
-    screen[row]![col] = cycleShowsSprite() ? 'X' : '.';
-}
-
-function cycleShowsSprite(): boolean {
-    let cycleCol = (cycle-1) % 40;
-    return (regX === cycleCol || (regX - 1) === cycleCol || (regX + 1) === cycleCol);
-}
-
-function renderScreen(): void {
-    for(let row of screen) {
-        console.log(row.join(''));
-    }
 }
