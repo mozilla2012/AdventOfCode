5f084b08a98573bd7072896f69fa5ca7107d8d31
mozilla2012 <earnold1993@gmail.com>
2022-12-09 22:52:28 -0800

    2022 10 Part 2

--- 2022/days/10/10.ts
+++ 2022/days/10/10.ts
@@ -3,11 +3,13 @@
 let regX: number;
 let cycle: number;
 let sumStrength: number;
+let screen: string[][];
 
 export function adventMain(input: string): any {
     regX = 1;
     cycle = 1;
     sumStrength = 0;
+    screen = [new Array(40), new Array(40), new Array(40), new Array(40), new Array(40), new Array(40)];
     const lines = input.split('\n');
 
     for(let line of lines) {
@@ -21,15 +23,32 @@ export function adventMain(input: string): any {
             regX += val;
         }
     }
+    renderScreen();
     return sumStrength;
 }
 
 function incrementCycleAndCalcStrength(): void{
+    updateScreen();
     if( ((cycle + 20) % 40) === 0 && cycle <= 220) {
-        console.log(cycle);
         let strength = (regX * cycle);
-        console.log(strength);
         sumStrength += strength;
     }
     cycle++;
+}
+
+function updateScreen(): void {
+    let row = Math.floor((cycle-1) / 40);
+    let col = (cycle-1) % 40;
+    screen[row]![col] = cycleShowsSprite() ? 'X' : '.';
+}
+
+function cycleShowsSprite(): boolean {
+    let cycleCol = (cycle-1) % 40;
+    return (regX === cycleCol || (regX - 1) === cycleCol || (regX + 1) === cycleCol);
+}
+
+function renderScreen(): void {
+    for(let row of screen) {
+        console.log(row.join(''));
+    }
 }
