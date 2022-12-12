763f1f0ccb598a0ebb848d1b2be12cde293755af
mozilla2012 <earnold1993@gmail.com>
2022-12-12 00:23:19 -0800

    Minifying/golfing 2022 12 Part 2

--- 2022/days/12/12.ts
+++ 2022/days/12/12.ts
@@ -1,5 +1,4 @@
 // https://adventofcode.com/2022/day/12
-// let shortestPath: number;
 
 export function adventMain(input: string): any {
     const lines = input.split('\n');
@@ -20,15 +19,18 @@ export function adventMain(input: string): any {
             } else {
                 intRow.push(lines[row]!.charCodeAt(col) - 'a'.charCodeAt(0) + 1);
             }
-            distRow.push(9999);
+            distRow.push(Number.MAX_VALUE);
         }
         heights.push(intRow);
         distance.push(distRow);
     }
 
-    distance[start[0]]![start[1]] = 0;
     let cellsToPaintNext: [number, number][] = [];
 
+    // For Part 1
+    // cellsToPaintNext.push(start);
+    // distance[start[0]]![start[1]] = 0;
+    // For Part 2:
     for(let row = 0; row < lines.length; row++) {
         for (let col = 0; col < lines[0]!.length; col++) {
             if(heights[row]![col]! === 1) {
@@ -45,40 +47,24 @@ export function adventMain(input: string): any {
     return distance[goal[0]]![goal[1]]!;
 }
 
-function paintNeighbors(current: [number, number], heights: number[][], distance: number[][]): [number,number][] {
-    const curRow: number = current[0];
-    const curCol: number = current[1];
-    const curHeight: number = heights[curRow]![curCol]!;
-    const curDistance: number = distance[curRow]![curCol]!;
-    let cellsToPaintNext: [number, number][] = [];
-    // Then traverse...
-    if((curRow-1) >= 0 // Spot is in grid
-        && ( heights[curRow-1]![curCol]! <= (curHeight + 1)) // It is accessible
-        && (distance[curRow-1]![curCol]! > curDistance + 1)) { // Is the distance greater than the current or a peer?
-            distance[curRow-1]![curCol]! = curDistance + 1;
-            cellsToPaintNext.push([curRow-1, curCol]); // UP
-    }
-    if((curRow+1) <= (heights.length-1)  // Spot is in grid
-        && ( heights[curRow+1]![curCol]! <= (curHeight + 1)) // It is accessible
-        && (distance[curRow+1]![curCol]! > curDistance + 1)) { // Is the distance greater than the current or a peer?
-            distance[curRow+1]![curCol]! = curDistance + 1;
-            cellsToPaintNext.push([curRow+1, curCol]); // DOWN
-    }
-    if((curCol-1) >= 0  // Spot is in grid
-        && ( heights[curRow]![curCol-1]! <= (curHeight + 1)) // It is accessible
-        && (distance[curRow]![curCol-1]! > curDistance + 1)) { // Is the distance greater than the current or a peer?
-            distance[curRow]![curCol-1]! = curDistance + 1;
-            cellsToPaintNext.push([curRow, curCol-1]); // LEFT
-    }
-    if((curCol+1) <= (heights[0]!.length-1) // Spot is in grid
-        && ( heights[curRow]![curCol+1]! <= (curHeight + 1)) // It is accessible
-        && (distance[curRow]![curCol+1]! > curDistance + 1)) { // Is the distance greater than the current or a peer?
-            distance[curRow]![curCol+1]! = curDistance + 1;
-            cellsToPaintNext.push([curRow, curCol+1]); // RIGHT
-    }
-    return cellsToPaintNext;
-}
+function paintNeighbors(cur:[number, number], heights:number[][], dist:number[][]):[number, number][]{
+	let cellsToPaintNext:[number, number][]=[];
 
-function arePointsEqual(x: [number, number], y: [number, number]) {
-    return ((x[0] === y[0]) && (x[1] === y[1]));
+	if((cur[0]-1)>=0&&(heights[cur[0]-1]![cur[1]]!<=((heights[cur[0]]![cur[1]]!)+1))&&(dist[cur[0]-1]![cur[1]]!>(dist[cur[0]]![cur[1]]!)+1)){
+		dist[cur[0]-1]![cur[1]]! = (dist[cur[0]]![cur[1]]!)+1;
+		cellsToPaintNext.push([cur[0]-1, cur[1]]);
+	}
+	if((cur[0]+1)<=(heights.length-1)&&(heights[cur[0]+1]![cur[1]]!<=((heights[cur[0]]![cur[1]]!)+1))&&(dist[cur[0]+1]![cur[1]]!>(dist[cur[0]]![cur[1]]!)+1)){
+		dist[cur[0]+1]![cur[1]]! = (dist[cur[0]]![cur[1]]!)+1;
+		cellsToPaintNext.push([cur[0]+1, cur[1]]);
+	}
+	if((cur[1]-1)>=0&&(heights[cur[0]]![cur[1]-1]!<=((heights[cur[0]]![cur[1]]!)+1))&&(dist[cur[0]]![cur[1]-1]!>(dist[cur[0]]![cur[1]]!)+1)){
+		dist[cur[0]]![cur[1]-1]! = (dist[cur[0]]![cur[1]]!)+1;
+		cellsToPaintNext.push([cur[0], cur[1]-1]);
+	}
+	if((cur[1]+1)<=(heights[0]!.length-1)&&(heights[cur[0]]![cur[1]+1]!<=((heights[cur[0]]![cur[1]]!)+1))&&(dist[cur[0]]![cur[1]+1]!>(dist[cur[0]]![cur[1]]!)+1)){
+		dist[cur[0]]![cur[1]+1]! = (dist[cur[0]]![cur[1]]!)+1;
+		cellsToPaintNext.push([cur[0], cur[1]+1]);
+	}
+	return cellsToPaintNext;
 }
