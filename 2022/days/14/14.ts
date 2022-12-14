085a2371b1484c908e74c9992fcee1f975c356f3
mozilla2012 <earnold1993@gmail.com>
2022-12-13 23:18:52 -0800

    Minifying/golfing 2022 14 Part 2

--- 2022/days/14/14.ts
+++ 2022/days/14/14.ts
@@ -1,46 +1,31 @@
 // https://adventofcode.com/2022/day/14
 
-const SAND = 2;
-const ROCK = 1;
-const VOID = 0;
-
 export function adventMain(input: string): any {
     const lines = input.split('\n');
     // First, read in all lines. Find the largest X and Y values.
     let maxRow = 0;
-    let maxCol = 0;
     let rockFormations: number[][][] = [];
     for(let inputLine of lines) {
         let strRockLine: string[] = inputLine.split(' -> ');
         let rockLine: number[][] = [];
         for(let strPoint of strRockLine) {
             let point: number[] = strPoint.split(',').map(strNum => parseInt(strNum)); // point[] = [col, row]
-            let row = point[1]!;
-            let col = point[0]!;
-            if(row > maxRow) { maxRow = row; }
-            if(col > maxCol) { maxCol = col; }
+            if((point[1]!) > maxRow) { maxRow = (point[1]!); }
             rockLine.push(point);
         }
         rockFormations.push(rockLine);
     }
 
     // Part 2
-    maxCol = 999999; 
-    rockFormations.push(
-        [
-            [0, maxRow+2],
-            [maxCol, maxRow+2]
-        ]
-    );
-    maxRow = maxRow+2;
+    rockFormations.push([[0, maxRow+2], [999999, maxRow+2]]);
+    maxRow += 2;
     // end Part 2
 
     // Then initialize a grid of size maxRow * maxCol and draw the rock lines.
     let cave: number[][] = [];
-
     for(let row = 0; row < maxRow+2; row++) { // Add two to the height just to be safe
         let line = [];
-        for(let col = 0; col < maxCol+2; col++) { // Add two to the width just to be safe
+        for(let col = 0; col < 999999+2; col++) { // Add two to the width just to be safe
             line.push(0);
         }
         cave.push(line);
@@ -52,70 +37,46 @@ export function adventMain(input: string): any {
 function drawRockLines(cave: number[][], rockFormations: number[][][]): number[][] {
     for(let formation of rockFormations) {
         for(let i = 0; i < formation.length - 1; i++) {
-            let point1 = formation[i]!;
-            let point2 = formation[i+1]!;
-            let row1 = point1[1]!;
-            let col1 = point1[0]!;
-            let row2 = point2[1]!;
-            let col2 = point2[0]!;
-            cave[row1]![col1] = ROCK;
-            cave[row2]![col2] = ROCK;
-            if(row1 === row2) {
+            cave[((formation[i]!)[1]!)]![((formation[i]!)[0]!)] = 1;
+            cave[((formation[i+1]!)[1]!)]![((formation[i+1]!)[0]!)] = 1;
+            if(((formation[i]!)[1]!) === ((formation[i+1]!)[1]!)) {
                 // Draw a horizontal line
-                let coli = col1;
+                let coli = ((formation[i]!)[0]!);
                 do {
-                    cave[row1]![coli] = ROCK;
-                    coli = (col1 < col2) ? coli + 1 : coli - 1;
-                } while(coli !== col2);
-            } else if(col1 === col2) {
+                    cave[((formation[i]!)[1]!)]![coli] = 1;
+                    coli = (((formation[i]!)[0]!) < ((formation[i+1]!)[0]!)) ? coli + 1 : coli - 1;
+                } while(coli !== ((formation[i+1]!)[0]!));
+            } else if(((formation[i]!)[0]!) === ((formation[i+1]!)[0]!)) {
                 // Draw a vertical line
-                let rowi = row1;
+                let rowi = ((formation[i]!)[1]!);
                 do {
-                    cave[rowi]![col1] = ROCK;
-                    rowi = (row1 < row2) ? rowi + 1 : rowi - 1;
-                } while(rowi !== row2);
+                    cave[rowi]![((formation[i]!)[0]!)] = 1;
+                    rowi = (((formation[i]!)[1]!) < ((formation[i+1]!)[1]!)) ? rowi + 1 : rowi - 1;
+                } while(rowi !== ((formation[i+1]!)[1]!));
             }
         }
     }
     return cave;
 }
 
-function simulateCave(cave: number[][]): number {    
-    const sandOriginRow = 0;
-    const sandOriginCol = 500;
+function simulateCave(cave: number[][]): number {
     while(true) { // Generate sand constantly
-        let sandRow = sandOriginRow;
-        let sandCol = sandOriginCol;
-
+        let sandRow = -1;
+        let sandCol = 500;
         while(true) { // Move the sand until it stops or falls off
-            if(sandRow + 1 === cave.length || cave[sandOriginRow]![sandOriginCol] === SAND) {
-                // WE HAVE SAND FALLING OFF! OR WE'RE PLUGGED UP!
-                return countSand(cave);
+            if(++sandRow + 1 === cave.length || cave[0]![500] === 2) {
+                // WE HAVE 2 FALLING OFF! OR WE'RE PLUGGED UP!
+                return cave.reduce((acc1, currentRow)=> acc1 + currentRow.reduce((acc2, section)=>acc2 + ((section === 2) ? 1 : 0)), -1);;
             }
-            if(cave[sandRow+1]![sandCol] === VOID) { // can move straight down, do so
-                sandRow++;
-            } else if (cave[sandRow+1]![sandCol-1] === VOID) { // can move down left, do so
-                sandRow++;
+            if(cave[sandRow+1]![sandCol] === 0) {}
+            else if (cave[sandRow+1]![sandCol-1] === 0) { // can move down left, do so
                 sandCol--;
-            } else if (cave[sandRow+1]![sandCol+1] === VOID) { // can move down right, do so
-                sandRow++;
+            } else if (cave[sandRow+1]![sandCol+1] === 0) { // can move down right, do so
                 sandCol++;
             } else { // CAN'T MOVE DOWN; FREEZE and make new sand
-                cave[sandRow]![sandCol] = SAND;
+                cave[sandRow]![sandCol] = 2;
                 break;
             }
         }
     }
-}
-
-function countSand(cave: number[][]): number {
-    let count = 0;
-    for(let row of cave) {
-        for (let section of row) {
-            if (section === SAND) {
-                count++;
-            }
-        }
-    }
-    return count;
 }
