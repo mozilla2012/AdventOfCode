cdd32dde42ee9a62f9041abe6975fc0d922b4c44
mozilla2012 <earnold1993@gmail.com>
2022-12-15 00:22:21 -0800

    2022 15 Part 1

--- 2022/days/15/15.ts
+++ 2022/days/15/15.ts
@@ -1,12 +1,5 @@
 // https://adventofcode.com/2022/day/15
 
-import { count } from "console";
-
-const UNKNOWN = 0;
-const BEACON = 1;
-const SENSOR = 2;
-const VOID = 3;
-
 let minX: number;
 let minY: number;
 let maxX: number;
@@ -35,25 +28,6 @@ export function adventMain(input: string): any {
         ]);
     }
 
-    const grid: number[][] = initGrid(allSensorBeaconPairs);
-    
-    populateDistances(allSensorBeaconPairs);
-    
-    
-    // printGrid(grid);
-
-
-    // console.log(allSensorBeaconPairs)
-
-    findVoids(allSensorBeaconPairs, grid);
-    // printGrid(grid);
-
-
-    return countVoidsForRow(grid, 10 + yOff);
-}
-
-function initGrid(allSensorBeaconPairs: [[number,number],[number,number],number][]): number[][] {
-    // console.log(allSensorBeaconPairs);
     let allX: number[] = [];
     let allY: number[] = [];
     allSensorBeaconPairs.forEach((pair)=>{
@@ -69,8 +43,7 @@ function initGrid(allSensorBeaconPairs: [[number,number],[number,number],number]
 
     const distX = Math.abs(maxX - minX);
     const distY = Math.abs(maxY - minY);
-    const bonusSpace = 0;//distX + distY;
-
+    const bonusSpace = distX + distY;
 
     minX -= bonusSpace;
     minY -= bonusSpace;
@@ -83,33 +56,26 @@ function initGrid(allSensorBeaconPairs: [[number,number],[number,number],number]
             thing[1] -= minY;
         }
     }
+
     yOff = minY * -1;
     maxX += (bonusSpace) - minX;
     maxY += (bonusSpace) - minY;
 
-    const grid: number[][] = [];
-    for(let y = 0; y <= maxY; y++) {
-        let row: number[] = [];
-        for(let x = 0; x <= maxX; x++) {
-            row.push(UNKNOWN);
+    populateDistances(allSensorBeaconPairs);
+
+    const targetRow = 2000000 + yOff;
+        let voids = 0;
+        for(let col = 0; col < maxX; col++) {
+            for(let pair of allSensorBeaconPairs) {
+                let sensor = pair[0];
+                let distanceToPoint = Math.abs(sensor[0]-col) + Math.abs(sensor[1]-targetRow);
+                if(distanceToPoint <= pair[2]) {
+                    voids++;
+                    break;
+                }
+            }
         }
-        grid.push(row);
-    }
-
-    for(let pair of allSensorBeaconPairs) {
-        let sen = pair[0];
-        let bec = pair[1];
-        // console.log(sen, bec);
-        grid[sen[1]]![sen[0]] = SENSOR; // ROW, COL
-        grid[bec[1]]![bec[0]] = BEACON;
-    }
-    return grid;    
-}
-
-function printGrid(grid: number[][]): void {
-    for(let row of grid) {
-        console.log(row.join('').replaceAll('0','.').replaceAll('1','B').replaceAll('2','S').replaceAll('3','#'));
-    }
+    return voids-1;
 }
 
 function populateDistances(allSensorBeaconPairs: [[number, number], [number, number], number][]) {
@@ -119,41 +85,3 @@ function populateDistances(allSensorBeaconPairs: [[number, number], [number, num
        pair[2] = Math.abs(sensor[0]-beacon[0]) + Math.abs(sensor[1]-beacon[1]);
     }
 }
-function findVoids(allSensorBeaconPairs: [[number, number], [number, number], number][], grid: number[][]) {
-    for(let pair of allSensorBeaconPairs) {
-        const sensor = pair[0];
-        // console.log(sensor);
-        const distance = pair[2];
-      
-        for(let row = sensor[1] - distance; row <= (sensor[1] + distance); row++) {
-            for(let col = sensor[0] - distance; col <= (sensor[0] + distance); col++) {
-                if(row >= 0 && row <= maxY && col >= 0 && col <= maxX) {
-                    let distanceToPoint = Math.abs(sensor[0]-col) + Math.abs(sensor[1]-row);
-                    if(distanceToPoint <= distance && grid[row]![col] === UNKNOWN) {
-                        grid[row]![col] = VOID;
-                    }
-                    
-                }
-            }
-        }
-        // printGrid(grid);
-        // console.log('');
-    }
-}
-
-function countVoidsForRow(grid: number[][], row: number): number {
-    let sum = 0;
-    let rowP: number[] = [];
-    for(let col = 0; col < maxX; col++) {
-        rowP.push(grid[row]![col]!);
-        if (grid[row]![col] === VOID) {
-            sum++;
-        }
-    }
-
-    // console.log('Row:')
-    // console.log(rowP.join('').replaceAll('0','.').replaceAll('1','B').replaceAll('2','S').replaceAll('3','#'));
-
-    return sum;
-}
-
