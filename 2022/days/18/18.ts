83d50df0c7a4eef66333f040c3e9dc6df2e8e62f
mozilla2012 <earnold1993@gmail.com>
2022-12-18 23:22:24 -0800

    2022 18 Part 2

--- 2022/days/18/18.ts
+++ 2022/days/18/18.ts
@@ -5,8 +5,8 @@ const ROCK = 0;
 const AIR = 1;
 const MAX = 21;
 export function adventMain(input: string): any {
+    // Parse input
     const lines = input.split('\n');
-    
     let grid: number[][][] = [];
     for(let x = 0; x <= MAX; x++) {
         let square = [];
@@ -19,7 +19,6 @@ export function adventMain(input: string): any {
         }
         grid.push(square);
     }
-
     for(let line of lines) {
         let v: string[] = line.split(',');
         let x = parseInt(v[0]!)+1;
@@ -27,43 +26,28 @@ export function adventMain(input: string): any {
         let z = parseInt(v[2]!)+1;
         grid[x]![y]![z]! = ROCK;
     }
+    // Determine what is external air
+    grid[0]![0]![0] = AIR;
+    markAllAir(grid);
+    // Count SA
     return getSurfaceArea(grid);
 }
 
 function getSurfaceArea(grid: number[][][]): number {
     let sa = 0;
     for(let x = 0; x <= MAX; x++) {
-        fillInAir(grid[x]!);
-        // printSquare(grid[x]!);
-    }
-    for(let x = 0; x <= MAX; x++) {
         for(let y = 0; y <= MAX; y++) {
             for(let z = 0; z <= MAX; z++) {
                 sa += calcSa(x, y, z, grid);
             }
-        }
+        }        
     }
     return sa;
 }
 
-function fillInAir(sq: number[][]) {
-    sq[0]![0] = AIR;
-    for(let x = 0; x <= MAX; x++) {
-        for(let y = 0; y <= MAX; y++) {
-            if (shouldBeAir(x, y, sq)) {
-                sq[x]![y] = AIR;
-            }
-        }
-    }
-}
-
-function shouldBeAir(x: number, y: number, sq: number[][]): boolean {
-    return (sq[x]![y]! === UNKNOWN);
-}
-
 function calcSa(x: number, y: number, z: number, grid: number[][][]): number {
     if(grid[x]![y]![z]! !== ROCK) {
-        return 0;
+        return 0; // Only count SA for rocks
     }
     let sa = 0;
     if (grid[x]![y]![z-1]! === AIR) { sa++; }
@@ -73,4 +57,51 @@ function calcSa(x: number, y: number, z: number, grid: number[][][]): number {
     if (grid[x-1]![y]![z]! === AIR) { sa++; }
     if (grid[x+1]![y]![z]! === AIR) { sa++; }
     return sa;
+}
+
+function markAllAir(grid: number[][][]): void {
+    let prevUnknown = Number.MAX_SAFE_INTEGER;
+    let currentUnknown = countUnknown(grid);
+    // This is stupid but I was having issues with recursion. For every space that is still UNKNOWN and any direct neighbors are air, mark the voxel as air. Repeat this process until the number of UNKNOWN voxels stops changing.
+    while(prevUnknown != currentUnknown) {
+        for(let x = 0; x <= MAX; x++) {
+            for(let y = 0; y <= MAX; y++) {
+                for(let z = 0; z <= MAX; z++) {
+                    if (grid[x]![y]![z] === UNKNOWN && anyNeighborsAreAir(x,y,z,grid)){
+                        grid[x]![y]![z]! = AIR;
+                    }
+                }
+            }
+        }
+        prevUnknown = currentUnknown;
+        currentUnknown = countUnknown(grid);
+    }
+}
+
+// Count how many voxels are "UNKNOWN" within the blob.
+function countUnknown(grid: number[][][]) {
+    let unknownCount = 0;
+    for(let x = 0; x <= MAX; x++) {
+        for(let y = 0; y <= MAX; y++) {
+            for(let z = 0; z <= MAX; z++) {
+                if (grid[x]![y]![z] === UNKNOWN){
+                    unknownCount++;
+                }
+            }
+        }
+    }
+    return unknownCount;
+}
+
+// Return true if on any edge or if any non-diagonal neighbor is air.
+function anyNeighborsAreAir(x: number, y: number, z: number, grid: number[][][]): boolean {
+    return ( x === 0 || y === 0 || z === 0 ||
+        x === MAX || y === MAX || z === MAX ||
+        grid[x+1]![y]![z]! === AIR ||
+        grid[x-1]![y]![z]! === AIR ||
+        grid[x]![y+1]![z]! === AIR ||
+        grid[x]![y-1]![z]! === AIR ||
+        grid[x]![y]![z+1]! === AIR ||
+        grid[x]![y]![z-1]! === AIR
+    );
 }
