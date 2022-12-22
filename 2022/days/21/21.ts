963932677356743a67ae1aaa0cec04c2346f6e83
mozilla2012 <earnold1993@gmail.com>
2022-12-21 22:25:38 -0800

    2022 21 Part 2

--- 2022/days/21/21.ts
+++ 2022/days/21/21.ts
@@ -1,4 +1,4 @@
-// https://adventofcode.com/2022/day/0
+// https://adventofcode.com/2022/day/21
 
 export function adventMain(input: string): any {
     let lines = input.split('\n');
@@ -7,13 +7,19 @@ export function adventMain(input: string): any {
         const matches = line.match(/([a-zA-Z]+): ([0-9]+)/);
         if(matches) {
             let monkey: string = matches[1]!;
+            if (monkey === 'humn') { // Part 2
+                continue;
+            }
             let number: string = matches[2]!;
             lines = lines.map(line => line.replace(monkey, number));
         }
     }
 
-    while(true) {
+    let prevLines: string[] = [];
+    while(prevLines !== lines) {
+        prevLines = lines;
         for(let line of lines) {
+            // Then go through and calculate any completed lines.
             const matches = line.match(/([a-zA-Z]+): ([0-9]+) (.) (([0-9]+))/);
             if(matches) {
                 let monkey: string = matches[1]!;
@@ -24,11 +30,65 @@ export function adventMain(input: string): any {
                     (op === '-') ? (op1-op2) : (
                     (op === '*') ? (op1*op2) : (
                     (op === '/') ? (op1/op2) : (Number.MAX_SAFE_INTEGER))));
-                if (monkey === 'root') {
-                    return number;
-                }
+                // if (monkey === 'root') { // Part 1
+                //     return number;
+                // }
+                // Then replace the monkey name with the calculated value.
                 lines = lines.map(line => line.replace(monkey, `${number}`));
             }
         }
     }
+    
+    // At this point, everything that *can* be substituted has been.
+    // The root monkey should have one of the two operands populated.
+    
+    let rootMonkeyLine = getMonkeyLineByName('root', lines);
+
+    // From here, rootMonkeyLine is like 'root: xyzb + 123456'
+    // Then we can determine that xyzb must be 123456. 
+    // Then we go loop repeatedly: Everywhere we find the monkey name, replace it with that value.
+    // Then repeat: Extract the value that wasn't determined, determine it's new value, then replace it.
+    let rootMatch = rootMonkeyLine.match(/([a-zA-Z]+): ([a-zA-Z]+) (.) ([0-9]+)/);
+    let valToReplace = parseInt(rootMatch![4]!);
+    let monkeyToFind = rootMatch![2]!;
+    
+    while(monkeyToFind !== 'humn') {
+        // Substitute all lines that contain the calculated value.
+        lines = lines.map(line => line.replace(monkeyToFind, `${valToReplace}`));
+        // Then find the new valToReplace and MonkeyToFind.
+        for(let line of lines) {
+            let leftVarMatch = line.match(/([0-9]+): ([a-zA-Z]+) (.) ([0-9]+)/); // monkeyName: monkeyName + 12345
+            if(leftVarMatch) {
+                let value = parseInt(leftVarMatch[4]!);
+                monkeyToFind = leftVarMatch[2]!;
+                let op = leftVarMatch[3]!;
+                valToReplace = (op === '+') ? (valToReplace - value) : (
+                    (op === '-') ? (valToReplace + value) : (
+                    (op === '*') ? (valToReplace / value) : (
+                    (op === '/') ? (valToReplace * value) : (Number.MAX_SAFE_INTEGER))));
+                break;
+            }
+            let rightVarMatch = line.match(/([0-9]+): ([0-9]+) (.) ([a-zA-Z]+)/); // monkeyName: 12345 + monkeyName
+            if(rightVarMatch) {
+                let value = parseInt(rightVarMatch[2]!);
+                monkeyToFind = rightVarMatch[4]!;
+                let op = rightVarMatch[3]!;
+                valToReplace = (op === '+') ? (valToReplace - value) : (
+                    (op === '-') ? (value - valToReplace) : (
+                    (op === '*') ? (valToReplace / value) : (
+                    (op === '/') ? (value / valToReplace) : (Number.MAX_SAFE_INTEGER))));
+                break;
+            }
+        }
+    }
+    return valToReplace;
+}
+
+function getMonkeyLineByName(monkeyName: string, lines: string[]): string {
+    for (let line of lines) {
+        if (line.includes(`${monkeyName}:`)) {
+            return line;
+        }
+    }
+    return '';
 }
