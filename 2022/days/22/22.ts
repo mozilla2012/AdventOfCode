85edbbdff81476377c1db3ceacd650bb2a31be2b
mozilla2012 <earnold1993@gmail.com>
2022-12-27 20:13:10 -0800

    2022 22 Part 2 - Refactor

--- 2022/days/22/22.ts
+++ 2022/days/22/22.ts
@@ -36,10 +36,9 @@ export function adventMain(input: string): any {
         let cols = line.split('');
         
         for(let col = 0; col < cols.length; col++) {
-            const sq = (cols[col] === ' ') ? VOID : 
-                       (cols[col] === '.') ? OPEN :
-                       (cols[col] === '#') ? WALL : VOID;
-            grid[row]![col]! = sq;
+            grid[row]![col]! = (cols[col] === ' ') ? VOID : 
+                               (cols[col] === '.') ? OPEN :
+                               (cols[col] === '#') ? WALL : VOID;
         }
         row++;
     }
@@ -51,7 +50,7 @@ export function adventMain(input: string): any {
     curRow = 0;
     curCol = 0;
     curDir = RIGHT;
-    while(grid[curRow]![curCol] !== OPEN) {
+    while(grid[curRow]![curCol] !== OPEN) { // Find the first location.
         curCol++;
     }
 
@@ -70,11 +69,9 @@ export function adventMain(input: string): any {
         // Then parse and set direction (get next char)
         let newDir = `${instructions}`.charAt(0);
         instructions = instructions.slice(1);
-        if (newDir === 'R') {
-            curDir = (curDir === 3) ? 0 : (curDir + 1);
-        } else {
-            curDir = (curDir === 0) ? 3 : (curDir - 1);
-        }
+        curDir = (newDir === 'R') ? 
+                 (curDir === 3) ? 0 : (curDir + 1) :
+                 (curDir === 0) ? 3 : (curDir - 1);
     }
 
     // Calculate the return value
@@ -126,153 +123,62 @@ function moveIfPossibleCube(grid: number[][]): boolean {
     exit();
 }
 
-// function translateToNewSideTest(grid: number[][]) {
-//     if(curRow === 0 && curDir === UP) { // Side 1 -> 12
-//         curDir = DOWN;
-//         curRow = 4;
-//         let edgeIndex = curCol % 4;
-//         curCol = (4-1) - edgeIndex;
-//     } else if (curRow < 4 && curCol === (12-1) && curDir === RIGHT) { // Side 2 -> 5
-//         curDir = LEFT;
-//         curCol = (16-1);
-//         let edgeIndex = curRow % 4;
-//         curRow = (12-1) - edgeIndex;
-//     } else if (curRow >= 4  && curCol === (12-1) && curDir === RIGHT) { // Side 3 -> 4
-//         curDir = DOWN;
-//         let edgeIndex = curRow % 4;
-//         curCol = (16-1) - edgeIndex;
-//         curRow = 8;
-//     } else if (curRow === 8 && curDir === UP) { // Side 4 -> 3
-//         curDir = LEFT;
-//         let edgeIndex = curCol % 4;
-//         curRow = (8-1) - edgeIndex;
-//         curCol = (12-1);
-//     } else if (curCol === (16-1) && curDir === RIGHT) { // Side 5 -> 2
-//         curDir = LEFT;
-//         curCol = (12-1);
-//         let edgeIndex = curRow % 4;
-//         curRow = (4-1) - edgeIndex;
-//     } else if (curRow === (12-1) && curCol >= 12 && curDir === DOWN) { // Side 6 -> 11
-//         curDir = RIGHT;
-//         let edgeIndex = curCol % 4;
-//         curRow = (8-1) - edgeIndex; 
-//         curCol = 0;
-//     } else if (curRow === (12-1) && curCol <  12 && curDir === DOWN) { // Side 7 -> 10
-//         curDir = UP;
-//         let edgeIndex = curCol % 4;
-//         curCol = (4-1) - edgeIndex; 
-//         curRow = (8-1);
-//     } else if (curRow < 4 && curCol === 8 && curDir === LEFT) { // Side 8 -> 9
-//         curDir = UP;
-//         let edgeIndex = curRow % 4;
-//         curCol = (8-1) - edgeIndex; 
-//         curRow = (8-1);
-//     } else if (curRow === (8-1)  && curCol >=  4 && curDir === DOWN) { // Side 9 -> 8
-//         curDir = RIGHT;
-//         let edgeIndex = curCol % 4;
-//         curRow = (12-1) - edgeIndex; 
-//         curCol = (8-1);
-//     } else if (curRow === (8-1)  && curCol <   4 && curDir === DOWN) { // Side 10 -> 7
-//         curDir = UP;
-//         let edgeIndex = curCol % 4;
-//         curCol = (12-1) - edgeIndex; 
-//         curRow = (12-1);
-//     } else if (curCol === 0 && curDir === LEFT) { // Side 11 -> 6
-//         curDir = UP;
-//         let edgeIndex = curRow % 4;
-//         curCol = (16-1) - edgeIndex; 
-//         curRow = (12-1);
-//     } else if (curRow === 4 && curCol < 4 && curDir === UP) { // Side 12 -> 1
-//         curDir = DOWN;
-//         let edgeIndex = curCol % 4;
-//         curCol = (12-1) - edgeIndex; 
-//         curRow = 0;
-//     } else if (curRow === 4 && curCol >= 4 && curDir === UP) { // Side 13 -> 14
-//         curDir = RIGHT;
-//         let edgeIndex = curCol % 4;
-//         curRow = edgeIndex; 
-//         curCol = 8;
-//     } else if (curRow >= 8 && curCol === 8 && curDir === LEFT) { // Side 14 -> 13
-//         curDir = DOWN;
-//         let edgeIndex = curRow % 4;
-//         curCol = (8-1) - edgeIndex; 
-//         curRow = 4;
-//     } else {
-//         console.log(`Unknown rollover! Currently at [${curRow},${curCol}], heading ${curDir}`);
-//     }
-// }
-
 function translateToNewSide(grid: number[][]) {
-    
     if(curRow === 0 && curCol < 100 && curDir === UP) { // Side 1 -> 10
         curDir = RIGHT;
-        let edgeIndex = curCol % 50;
-        curRow = 150 + edgeIndex; 
+        curRow = 150 + (curCol % 50); 
         curCol = 0;
     } else if (curRow === 0 && curCol >= 100 && curDir === UP) { // Side 2 -> 9
         curDir = UP;
-        let edgeIndex = curCol % 50;
-        curCol = 0 + edgeIndex; // Not sure if this should be inverted...
+        curCol = 0 + (curCol % 50); // Not sure if this should be inverted...
         curRow = (200-1);
     } else if (curCol === (150-1) && curDir === RIGHT) { // Side 3 -> 6
         curDir = LEFT;
-        let edgeIndex = curRow % 50;
-        curRow = (150-1) - edgeIndex; 
+        curRow = (150-1) - (curRow % 50); 
         curCol = (100-1);
     } else if (curRow === (50-1) && curDir === DOWN) { // Side 4 -> 5
         curDir = LEFT;
-        let edgeIndex = curCol % 50;
-        curRow = 50 + edgeIndex; 
+        curRow = 50 + (curCol % 50); 
         curCol = (100-1);
     } else if (curRow < 100 && curCol === (100-1) && curDir === RIGHT) { // Side 5 -> 4
         curDir = UP;
-        let edgeIndex = curRow % 50;
-        curCol = 100 + edgeIndex; 
+        curCol = 100 + (curRow % 50); 
         curRow = (50-1);
     } else if (curRow >= 100 && curCol === (100-1) && curDir === RIGHT) { // Side 6 -> 3
         curDir = LEFT;
-        let edgeIndex = curRow % 50;
-        curRow = (50-1) - edgeIndex; 
+        curRow = (50-1) - (curRow % 50); 
         curCol = (150-1);
     } else if (curRow === (150-1) && curCol > 40 && curDir === DOWN) { // Side 7 -> 8
         curDir = LEFT;
-        let edgeIndex = curCol % 50;
-        curRow = 150 + edgeIndex; 
+        curRow = 150 + (curCol % 50); 
         curCol = (50-1);
     } else if (curRow > 140 && curCol === (50-1) && curDir === RIGHT) { // Side 8 -> 7
         curDir = UP;
-        let edgeIndex = curRow % 50;
-        curCol = 50 + edgeIndex; 
+        curCol = 50 + (curRow % 50); 
         curRow = (150-1);
     } else if (curRow === (200-1) && curDir === DOWN) { // Side 9 -> 2
         curDir = DOWN;
-        let edgeIndex = curCol % 50;
-        curCol = 100 + edgeIndex; 
+        curCol = 100 + (curCol % 50); 
         curRow = 0;
     } else if (curRow >= 150 && curCol === 0 && curDir === LEFT) { // Side 10 -> 1
         curDir = DOWN;
-        let edgeIndex = curRow % 50;
-        curCol = 50 + edgeIndex; 
+        curCol = 50 + (curRow % 50); 
         curRow = 0;
     } else if (curRow < 150 && curCol === 0 && curDir === LEFT) { // Side 11 -> 14
         curDir = RIGHT;
-        let edgeIndex = curRow % 50;
-        curRow = (50-1) - edgeIndex; 
+        curRow = (50-1) - (curRow % 50); 
         curCol = 50;
     } else if (curRow === 100 && curDir === UP) { // Side 12 -> 13
         curDir = RIGHT;
-        let edgeIndex = curCol % 50;
-        curRow = 50 + edgeIndex; 
+        curRow = 50 + (curCol % 50); 
         curCol = 50;
     } else if (curRow >= 50 && curCol === 50 && curDir === LEFT) { // Side 13 -> 12
         curDir = DOWN;
-        let edgeIndex = curRow % 50;
-        curCol = 0 + edgeIndex; 
+        curCol = 0 + (curRow % 50); 
         curRow = 100;
     } else if (curRow < 50 && curCol === 50 && curDir === LEFT) { // Side 14 -> 11
         curDir = RIGHT;
-        let edgeIndex = curRow % 50;
-        curRow = (150-1) - edgeIndex; 
+        curRow = (150-1) - (curRow % 50); 
         curCol = 0;
     } else {
         console.log(`Unknown rollover! Currently at [${curRow},${curCol}], heading ${curDir}`);
