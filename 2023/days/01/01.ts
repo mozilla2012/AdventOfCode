a849876fc30db2aa4e52dcc9fcac3c2ddc78ff04
mozilla2012 <earnold1993@gmail.com>
2023-11-30 21:34:03 -0800

    2023 01 Part 2

--- 2023/days/01/01.ts
+++ 2023/days/01/01.ts
@@ -2,9 +2,23 @@
 
 const FIRST_NON_DIGIT_ASCII = 58;
 
+const numbers = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'zero',
+]
+
 export function adventMain(input: string): any {
     const lines: string[] = input.split('\n');
     let sum = 0;
+
     for(let line of lines) {
         const len = line.length;
         let left;
@@ -14,16 +28,59 @@ export function adventMain(input: string): any {
             if (charValue < FIRST_NON_DIGIT_ASCII) {
                 left = line.charAt(i);
                 break;
+            } else {
+                for(let n = 0; n < numbers.length; n++) {
+                    if(line.slice(i).startsWith(numbers[n]!)) {
+                        left = n + 1;
+                        break;
+                    }
+                }
             }
+            if (left != undefined) { break; }
         }
         for (let i = len-1; i >= 0; i--) {
             const charValue: number = line.charCodeAt(i);
             if (charValue < FIRST_NON_DIGIT_ASCII) {
                 right = line.charAt(i);
                 break;
+            } else {
+                for(let n = 0; n < numbers.length; n++) {
+                    if(line.slice(i).startsWith(numbers[n]!)) {
+                        right = n + 1;
+                        break;
+                    }
+                }
             }
+            if (right != undefined) { break; }
         }
         sum += Number(left) * 10 + Number(right);
     }
     return sum;
 }
+
+// Part 1
+// export function adventMain(input: string): any {
+//     const lines: string[] = input.split('\n');
+//     let sum = 0;
+//     for(let line of lines) {
+//         const len = line.length;
+//         let left;
+//         let right;
+//         for (let i = 0; i < len; i++) {
+//             const charValue: number = line.charCodeAt(i);
+//             if (charValue < FIRST_NON_DIGIT_ASCII) {
+//                 left = line.charAt(i);
+//                 break;
+//             }
+//         }
+//         for (let i = len-1; i >= 0; i--) {
+//             const charValue: number = line.charCodeAt(i);
+//             if (charValue < FIRST_NON_DIGIT_ASCII) {
+//                 right = line.charAt(i);
+//                 break;
+//             }
+//         }
+//         sum += Number(left) * 10 + Number(right);
+//     }
+//     return sum;
+// }
