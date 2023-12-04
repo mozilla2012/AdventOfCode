c9ec28dbad368c6f3f0214df994dad30febab621
mozilla2012 <earnold1993@gmail.com>
2023-12-03 22:30:34 -0800

    2023 04 Part 2 golfed

--- 2023/days/04/04.ts
+++ 2023/days/04/04.ts
@@ -1,20 +1,28 @@
 // https://adventofcode.com/2023/day/4
 
-export function adventMain(input: string): any {
-    const lines = input.split('\n');
-    let numCards = Array<number>(lines.length).fill(1);
-
-    for(let l = 0; l < lines.length; l++) {
-        const card: string = lines[l]!.split(':')[1]!;
-        const magicNumbers: number[] = (card.split('|')[0])!.split(' ').filter((t) => !!t).map((s) => parseInt(s));
-        const myNumbers: number[] = (card.split('|')[1])!.split(' ').filter((t) => !!t).map((s) => parseInt(s));
-        const numMatches = magicNumbers.filter(n => myNumbers.includes(n)).length;
-        for(let i = 1; i< numMatches + 1; i++) {
-            numCards[l + i] += numCards[l]!;
-        }
-    }
+// Part 2 golfed
+export function adventMain(p: string): any {
+    let numCards = Array<number>(p.split('\n').length).fill(1);
+    p.split('\n').forEach((e, l) => { for(let i = 1; i< ((e!.split(':')[1]!.split('|')[0])!.trim().split(/\s+/)).filter(n => ((e.split(':')[1]!.split('|')[1])!.trim().split(/\s+/)).includes(n)).length + 1; i++) { numCards[l + i] += numCards[l]! }});
     return numCards.reduce((sum, value) => sum + value);
 }
+
+// Part 2
+// export function adventMain(input: string): any {
+//     const lines = input.split('\n');
+//     let numCards = Array<number>(lines.length).fill(1);
+
+//     for(let l = 0; l < lines.length; l++) {
+//         const card: string = lines[l]!.split(':')[1]!;
+//         const magicNumbers: number[] = (card.split('|')[0])!.split(' ').filter((t) => !!t).map((s) => parseInt(s));
+//         const myNumbers: number[] = (card.split('|')[1])!.split(' ').filter((t) => !!t).map((s) => parseInt(s));
+//         const numMatches = magicNumbers.filter(n => myNumbers.includes(n)).length;
+//         for(let i = 1; i< numMatches + 1; i++) {
+//             numCards[l + i] += numCards[l]!;
+//         }
+//     }
+//     return numCards.reduce((sum, value) => sum + value);
+// }
 
 // Part 1
 // export function adventMain(input: string): any {
