36dc48926edb3fd7eff06383332afbfba628421e
mozilla2012 <earnold1993@gmail.com>
2023-12-04 22:08:41 -0800

    2023 05 Part 2

--- 2023/days/05/05.ts
+++ 2023/days/05/05.ts
@@ -2,7 +2,7 @@
 
 export function adventMain(input: string): any {
     const lines = input.split('\n');
-    let seeds: number[] = lines[0]!.split(':')[1]!.trim().split(' ').map((s) => parseInt(s));
+    let seedRanges: number[] = lines[0]!.split(':')[1]!.trim().split(' ').map((s) => parseInt(s));
     let maps: number[][][] = [];
 
     for(let i = 2; i < lines.length - 1; i++) {
@@ -18,13 +18,15 @@ export function adventMain(input: string): any {
 
     const startingPosition = -1;
     let smallestLocation = startingPosition;
-    for(let i = 0; i < seeds.length; i++) {
-        let valToTrace = seeds[i]!;
-        for(let map of maps) {
-            valToTrace = findNewValInMap(map, valToTrace);
-        }
-        if (valToTrace < smallestLocation || smallestLocation == startingPosition) {
-            smallestLocation = valToTrace;
+    for(let j = 0; j < seedRanges.length; j += 2) {
+        for(let i = seedRanges[j]!; i < seedRanges[j+1]! + seedRanges[j]!; i++) {
+            let valToTrace = i;
+            for(let map of maps) {
+                valToTrace = findNewValInMap(map, valToTrace);
+            }
+            if (valToTrace < smallestLocation || smallestLocation == startingPosition) {
+                smallestLocation = valToTrace;
+            }
         }
     }
     return smallestLocation;
@@ -40,3 +42,43 @@ function findNewValInMap(mapPack: number[][], val: number): number {
     return val;
 }
 
+// Part 1
+// export function adventMain(input: string): any {
+//     const lines = input.split('\n');
+//     let seeds: number[] = lines[0]!.split(':')[1]!.trim().split(' ').map((s) => parseInt(s));
+//     let maps: number[][][] = [];
+
+//     for(let i = 2; i < lines.length - 1; i++) {
+//         let line = lines[++i]!;
+//         let newMap: number[][] = [];
+//         while(i < lines.length && line.length > 0) {
+//             const numberMap: number[] = line.split(' ').map((s) => parseInt(s));
+//             newMap.push(numberMap);
+//             line = lines[++i]!;
+//         }
+//         maps.push(newMap);
+//     }
+
+//     const startingPosition = -1;
+//     let smallestLocation = startingPosition;
+//     for(let i = 0; i < seeds.length; i++) {
+//         let valToTrace = seeds[i]!;
+//         for(let map of maps) {
+//             valToTrace = findNewValInMap(map, valToTrace);
+//         }
+//         if (valToTrace < smallestLocation || smallestLocation == startingPosition) {
+//             smallestLocation = valToTrace;
+//         }
+//     }
+//     return smallestLocation;
+// }
+
+// function findNewValInMap(mapPack: number[][], val: number): number {
+//     for(let map of mapPack) {
+//         if(val >= map[1]! && val < map[1]! + map[2]!) {
+//             const calcVal = (map[0]! + (val - map[1]!));
+//             return calcVal;
+//         }
+//     }
+//     return val;
+// }
