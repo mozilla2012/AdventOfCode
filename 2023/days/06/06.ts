dd0a44fff618e84aa7f8947bcf23101b744e3e5c
mozilla2012 <earnold1993@gmail.com>
2023-12-05 21:22:36 -0800

    2023 06 Part 2

--- 2023/days/06/06.ts
+++ 2023/days/06/06.ts
@@ -2,21 +2,36 @@
 
 export function adventMain(input: string): any {
     const lines = input.split('\n');
-    let times: number[] = lines[0]!.split(':')[1]!.trim().split(/\s+/).map((s) => parseInt(s));
-    let distances: number[] = lines[1]!.split(':')[1]!.trim().split(/\s+/).map((s) => parseInt(s));
-    let options: number[] = Array<number>(times.length).fill(0);
+    let time: number = parseInt(lines[0]!.split(':')[1]!.trim().replaceAll(' ', ''));
+    let distance: number = parseInt(lines[1]!.split(':')[1]!.trim().replaceAll(' ', ''));
+    let options: number = 0;
 
-    times.forEach((raceTime: number, index) => {
-        const distanceToBeat = distances[index]!;
-        for(let i = 0; i < raceTime; i++) {
-            const speed = i;
-            const remainingtime = raceTime - i;
-            const distanceTraveled = speed * remainingtime;
-            if (distanceTraveled > distanceToBeat) {
-                options[index]++;
-            }
+    for(let i = 0; i < time; i++) {
+        if (i * (time - i) > distance) {
+            options++;
         }
-    });
-
-    return options.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+    }
+    return options;
 }
+
+// Part 1
+// export function adventMain(input: string): any {
+//     const lines = input.split('\n');
+//     let times: number[] = lines[0]!.split(':')[1]!.trim().split(/\s+/).map((s) => parseInt(s));
+//     let distances: number[] = lines[1]!.split(':')[1]!.trim().split(/\s+/).map((s) => parseInt(s));
+//     let options: number[] = Array<number>(times.length).fill(0);
+
+//     times.forEach((raceTime: number, index) => {
+//         const distanceToBeat = distances[index]!;
+//         for(let i = 0; i < raceTime; i++) {
+//             const speed = i;
+//             const remainingtime = raceTime - i;
+//             const distanceTraveled = speed * remainingtime;
+//             if (distanceTraveled > distanceToBeat) {
+//                 options[index]++;
+//             }
+//         }
+//     });
+
+//     return options.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+// }
