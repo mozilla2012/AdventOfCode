c8d2fc2ba98d033eb1bbd2ed608364e29c3e2df6
mozilla2012 <earnold1993@gmail.com>
2023-12-07 22:05:40 -0800

    2023 08 Part 2; cleaning up

--- 2023/days/08/08.ts
+++ 2023/days/08/08.ts
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2023/day/8
 
-// Ripped off Stack Overflow: https://stackoverflow.com/questions/39899072/how-can-i-find-the-prime-factors-of-an-integer-in-javascript
+// Ripped this helper off Stack Overflow: https://stackoverflow.com/questions/39899072/how-can-i-find-the-prime-factors-of-an-integer-in-javascript
 function primeFactors(n: number): number[] {
     const factors = [];
     let divisor = 2;
@@ -20,36 +20,28 @@ export function adventMain(input: string): any {
     input = input.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '');
     const lines = input.split('\n');
     const directions = lines[0]!;
-    lines.shift();
-    lines.shift();
-
     const map = new Map<string, Array<string>>();
     const currentNode: string[] = [];
-
-    for(let line of lines) { // set up map
+    for(let line of lines.slice(2)) { // set up map
         const nodeName = line.split('=')[0]!;
         if (nodeName.charAt(2) === 'A') {
             currentNode.push(nodeName);
         }
         map.set(nodeName, [line.split('=')[1]!.split(',')[0]!, line.split('=')[1]!.split(',')[1]!])
     }
-
     const loopCount: number[] = currentNode.map(() => 0);
     for(let i = 0; i < currentNode.length; i++) { // iterate through map.
         while (currentNode[i]!.charAt(2) !== 'Z') {
             for(let direction of directions.split('')) {                
-                let currentDirections = map.get(currentNode[i]!)!;
-                currentNode[i] = direction === 'L' ? currentDirections[0]! : currentDirections[1]!;
+                currentNode[i] = direction === 'L' ? map.get(currentNode[i]!)![0]! : map.get(currentNode[i]!)![1]!;
                 loopCount[i]++;
             }   
         }
     }
-
-    const factorSet = new Set<number>();
+    const factorSet = new Set<number>(); // Figure out LCM
     loopCount.forEach((n) => primeFactors(n).forEach(s => factorSet.add(s)));
-    return Array.from(factorSet).reduce((acc, val) => acc*val);
+    return Array.from(factorSet).reduce((acc, val) => acc * val);
 }
-
 
 // Part 1
 // export function adventMain(input: string): any {
