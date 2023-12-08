5c1147e68be99e372fc6b40865e8d28f0cc9cd14
mozilla2012 <earnold1993@gmail.com>
2023-12-07 20:10:26 -0800

    2023 07 Part 2

--- 2023/days/07/07.ts
+++ 2023/days/07/07.ts
@@ -20,8 +20,19 @@ function determineHandType(cards: string): HandType {
             handMap.set(card, handMap.get(card)! + 1);
         }
     });
+    
+    // Count how many Jokers there are, then remove from the map.
+    const jCount: number = handMap.get('J') ?? 0;
+    handMap.delete('J');
+
     const counts: number[] = Array.from(handMap.values()).sort().reverse();
-    // console.log(counts);
+
+    if(counts.length === 0) {
+        counts.push(jCount);
+    } else {
+        counts[0] = counts[0]! + jCount;
+    }
+
     return (counts[0] === 5) ? HandType.FIVE :
            (counts[0] === 4) ? HandType.FOUR : 
            (counts[0] === 3 && counts[1] === 2) ? HandType.FULL : 
@@ -44,7 +55,7 @@ class Hand {
 }
 
 function compareCards(a: string, b: string): number {
-    const cardOrder = 'AKQJT987654321';
+    const cardOrder = 'AKQT987654321J';
     return cardOrder.indexOf(b) - cardOrder.indexOf(a);
 }
 
@@ -82,3 +93,100 @@ export function adventMain(input: string): any {
         return ((index + 1) * hand.bid) + runningTotal;
     }, 0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Part 1
+// enum HandType {
+//     FIVE = 6,
+//     FOUR = 5,
+//     FULL = 4,
+//     THREE = 3,
+//     TWOP = 2,
+//     ONEP = 1,
+//     HIGH = 0,
+//   }
+
+// function determineHandType(cards: string): HandType {
+//     // console.log(cards);
+//     const handMap = new Map<string, number>();
+//     cards.split('').forEach((card: string) => {
+//         if(!handMap.has(card)) {
+//             handMap.set(card, 1);
+//         } else {
+//             handMap.set(card, handMap.get(card)! + 1);
+//         }
+//     });
+//     const counts: number[] = Array.from(handMap.values()).sort().reverse();
+//     // console.log(counts);
+//     return (counts[0] === 5) ? HandType.FIVE :
+//            (counts[0] === 4) ? HandType.FOUR : 
+//            (counts[0] === 3 && counts[1] === 2) ? HandType.FULL : 
+//            (counts[0] === 3) ? HandType.THREE : 
+//            (counts[0] === 2 && counts[1] === 2) ? HandType.TWOP : 
+//            (counts[0] === 2) ? HandType.ONEP : 
+//            HandType.HIGH;
+// }
+
+// class Hand {
+//     cards: string;
+//     bid: number;
+//     type: HandType;
+
+//     constructor(cards: string, bid: number) {
+//         this.cards = cards;
+//         this.bid = bid;
+//         this.type = determineHandType(cards);
+//       }
+// }
+
+// function compareCards(a: string, b: string): number {
+//     const cardOrder = 'AKQJT987654321';
+//     return cardOrder.indexOf(b) - cardOrder.indexOf(a);
+// }
+
+// export function adventMain(input: string): any {
+//     const lines = input.split('\n');
+    
+//     // First, parse the hands.
+//     const hands: Hand[] = [];
+//     for(let line of lines) {
+//         const tokens = line.split(' ');
+//         hands.push(new Hand(tokens[0]!, parseInt(tokens[1]!)));
+//     }
+
+//     // Second, sort the hands.
+//     hands.sort((a: Hand, b: Hand) => {
+//         if (a.type === b.type) {
+//             // compare character by character
+//             let retVal = 0;
+//             a.cards.split('').some((_, i: number) => {
+//                 const cardA: string = a.cards[i]!;
+//                 const cardB: string = b.cards[i]!;
+//                 if(cardA !== cardB) {
+//                     retVal = compareCards(cardA, cardB);
+//                     return true;
+//                 }
+//                 return false;
+//             })
+//             return retVal;
+//         } 
+//         return a.type - b.type; // If B is better, return a positive number.
+//     });
+
+//     // Then, return the total.
+//     return hands.reduce((runningTotal: number, hand: Hand, index) => {
+//         return ((index + 1) * hand.bid) + runningTotal;
+//     }, 0);
+// }
