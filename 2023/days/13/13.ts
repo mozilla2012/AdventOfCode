fd0c3254a33085ac60842bb72de2ac0de4682b7d
mozilla2012 <earnold1993@gmail.com>
2023-12-13 00:12:54 -0800

    2023 13 Part 2

--- 2023/days/13/13.ts
+++ 2023/days/13/13.ts
@@ -1,5 +1,6 @@
 // https://adventofcode.com/2023/day/13
 
+const TARGET_DIFFERENCE = 1; // Change this to zero for part 1
 export function adventMain(input: string): any {
     const lines = input.split('\n');
     let grid: string[][] = [];
@@ -9,7 +10,7 @@ export function adventMain(input: string): any {
             sum += findValForGrid(grid);
             grid = [];
         } else {
-        grid.push(lines[i]!.split(''));     
+            grid.push(lines[i]!.split(''));
         }
     }
     return sum;
@@ -19,72 +20,36 @@ function transpose(thing: any[][]): any[][] {
     return thing[0]!.map((_, colIndex) => thing.map(row => row[colIndex]))
 }
 
-
-function printGrid(grid: string[][]) {
-    grid.forEach((row: string[]) => console.log(row.join('')));
+function countDifferingChars(s1: string, s2: string): number {
+    let diff = 0;
+    for(let i = 0; i < s1.length; i++) {
+        if(s1[i] !== s2[i]) {
+            diff++;
+        }
+    }
+    return diff;
 }
 
 function findValForGrid(grid: string[][]): number {
-    // console.log('Searching grid:');
-    // printGrid(grid);
     let retVal = 0;
-
-    // find vertical mirrors:
-    for(let i = 0; i < grid.length-1; i++) {
-        const row: string = grid[i]!.join('');
-        const nextRow: string = grid[i+1]!.join('');
-        if(row === nextRow) {
-            // Potential match found! Check for mirroring.
-            // Check below
-            let k = i;
-            let actualMirror = true;
-            for(let j = i+1; (j < grid.length && k >= 0); j++) {
-                const rowU: string = grid[k]!.join('');
-                const rowD: string = grid[j]!.join('');
-                // console.log('Comparing', rowU, rowD);
-                if(rowU !== rowD) {
-                    // console.log('Not a true mirror');
-                    actualMirror = false;
-                    break;
-                }
-                k--;
-            }
-            if (actualMirror) {
-                // console.log('adding1', 100*(i+1));
-                retVal += 100*(i+1);
-            }
+    for(let i = 0; i < grid.length-1; i++) { // find vertical mirrors:
+        let numDifferentCharacters = 0;
+        for(let j = i+1, k = i; (j < grid.length && k >= 0); j++, k--) {
+            numDifferentCharacters += countDifferingChars(grid[k]!.join(''), grid[j]!.join(''));
+        }
+        if (numDifferentCharacters === TARGET_DIFFERENCE) {
+            retVal += 100*(i+1);
         }
     }
-
-    // console.log('Done checking horizontal');
-    const transGrid = transpose(grid);
-    // Check horizontal mirror
+    const transGrid = transpose(grid);    // Check horizontal mirror
     for(let i = 0; i < transGrid.length-1; i++) {
-        const row: string = transGrid[i]!.join('');
-        const nextRow: string = transGrid[i+1]!.join('');
-        if(row === nextRow) {
-            // Potential match found! Check for mirroring.
-            // Check below
-            let k = i;
-            let actualMirror = true;
-            for(let j = i+1; (j < transGrid.length && k >= 0); j++) {
-                const rowU: string = transGrid[k]!.join('');
-                const rowD: string = transGrid[j]!.join('');
-                // console.log('Comparing', rowU, rowD);
-                if(rowU !== rowD) {
-                    // console.log('Not a true mirror');
-                    actualMirror = false;
-                    break;
-                }
-                k--;
-            }
-            if (actualMirror) {
-                // console.log('adding2', i+1);
-                retVal += i+1;
-            }
+        let numDifferentCharacters = 0;
+        for(let j = i+1, k = i; (j < transGrid.length && k >= 0); j++, k--) {
+            numDifferentCharacters += countDifferingChars(transGrid[k]!.join(''), transGrid[j]!.join(''));
+        }
+        if (numDifferentCharacters === TARGET_DIFFERENCE) {
+            retVal += i+1;
         }
     }
-
-    // console.log('Returning', retVal);
     return retVal;
 }
