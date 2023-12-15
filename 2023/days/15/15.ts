77687938da9e09b6386a7d2118a11e19413ed8aa
mozilla2012 <earnold1993@gmail.com>
2023-12-14 23:13:46 -0800

    2023 15 Part 2; golfing

--- 2023/days/15/15.ts
+++ 2023/days/15/15.ts
@@ -1,80 +1,26 @@
 // https://adventofcode.com/2023/day/15
 
 export function adventMain(input: string): any {
-    const lines = input.split('\n');
-    const line = lines[0]!;    
-
     const boxes: Map<number, string[]> = new Map();
-    for(let str of line.split(',')) {
+    for(let str of input.split(',')) {
         const label: string = str.split(/[=-]/)[0]!;
         const op: string = str.replace(label,'')[0]!;
-        let focal: number;
-        if (op === '=') {
-            focal = parseInt(str.replace(label,'')[1]!);
-        }
-        const box = hash(label);
-        if(op === '-' ){
-            if(boxes.has(box)) {
-                let lenses: string[] = boxes.get(box)!;
-                const existingIndex = lenses.findIndex((lens: string)=>{return lens.includes(label)});
-                if (existingIndex > -1) {
-                    lenses.splice(existingIndex, 1);
-                    if(lenses.length === 0){
-                        boxes.delete(box);
-                        continue;
-                    }
-                  }
-                boxes.set(box, lenses);
+        const box = label.split('').reduce((a, c) => ((a+c.charCodeAt(0)) * 17) % 256, 0);
+        if(op === '-' && boxes.has(box)){
+            boxes.set(box, boxes.get(box)!.filter(lens => !lens.includes(label)));
+        } else if(boxes.has(box)) {
+            const existingIndex = boxes.get(box)!.findIndex(lens => lens.includes(label));
+            if (existingIndex > -1) { // lens found
+                boxes.get(box)![existingIndex] = str;
+            } else { // If not found, append.
+                boxes.get(box)!.push(str);
             }
-            continue;
-        } else if(op === '=') {
-            if(boxes.has(box)) {
-                let lenses: string[] = boxes.get(box)!;
-                const existingIndex = lenses.findIndex((lens: string)=>{return lens.includes(label)});
-                if (existingIndex > -1) { // lens found
-                    lenses[existingIndex] = str;
-                } else {
-                    lenses.push(str);
-                }
-                boxes.set(box, lenses);
-                continue;
-            }
+        } else { // Make a new box if we're adding a new string.
             boxes.set(box, [str]);
-            continue;
         }
     }
-
-    let sum = 0;
-    return Array.from(boxes.keys()).reduce((acc2: number, box: number) => acc2 + boxes.get(box)!.reduce((acc, val, index) => acc + ( (box+1) * (index+1) * parseInt(val[val.length-1]!)), 0), 0);
-}
-
-function hash(str: string) {
-    let val = 0;
-    for (let i = 0; i < str.length; i++) {
-        val += str.charCodeAt(i);
-        val *= 17;
-        val %= 256;
-    }
-    return val;
+    return Array.from(boxes.keys()).reduce((a1, b) => a1 + boxes.get(b)!.reduce((a2, v, i) => a2 + ( (b+1) * (i+1) * parseInt(v[v.length-1]!)), 0), 0);
 }
 
 // Part 1
-// export function adventMain(input: string): any {
-//     const lines = input.split('\n');
-//     const line = lines[0]!;
-//     let sum = 0;
-//     for(let str of line.split(',')) {
-//         sum += hash(str);
-//     }
-//     return sum;
-// }
-
-// function hash(str: string) {
-//     let val = 0;
-//     for (let i = 0; i < str.length; i++) {
-//         val += str.charCodeAt(i);
-//         val *= 17;
-//         val %= 256;
-//     }
-//     return val;
-// }
+// export const adventMain = (i: string) => i.split(',').reduce((a1, s) => a1 + s.split('').reduce((a2, c) => ((a2 + c.charCodeAt(0)) * 17) % 256, 0), 0);
