a7536b1e96f28c94cb20e25883ec5f97a4b08d21
mozilla2012 <earnold1993@gmail.com>
2024-11-30 21:08:16 -0800

    2024 01 Part 1

---
+++ 2024/days/01/01.ts
@@ -0,0 +1,24 @@
+// https://adventofcode.com/2024/day/1
+
+export function adventMain(input: string): any {
+    const lines = input.split('\n');
+    const left: number[] = [];
+    const right: number[] = [];
+    lines.forEach((line: string) => {
+        const numbers: number[] = line.split('  ').map(s => Number(s));
+        left.push(numbers[0]!);
+        right.push(numbers[1]!);
+    });
+    left.sort();
+    right.sort();
+    console.log(left);
+    console.log(right);
+
+    let sum = 0;
+    for (let i=0; i < left.length; i++) {
+        const dist = Math.abs(left[i]!-right[i]!);
+        console.log(dist);
+        sum += dist;
+    }
+    return sum;
+}
