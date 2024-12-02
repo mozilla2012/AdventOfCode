2a4266ed941cc47000ae031882c5294982d113e0
mozilla2012 <earnold1993@gmail.com>
2024-12-01 21:23:46 -0800

    2024 02 Part 1

--- 2024/days/00/00.ts
+++ 2024/days/00/00.ts
@@ -1,5 +1,7 @@
 // https://adventofcode.com/2024/day/0
 
+import { Grid } from "../../util/Grid";
+
 export function adventMain(input: string): any {
     const lines = input.split('\n');
     lines.forEach((line: string) => {
---
+++ 2024/days/02/02.ts
@@ -0,0 +1,46 @@
+// https://adventofcode.com/2024/day/2
+
+import { Grid } from "../../util/Grid";
+import { p } from "../../util/utils";
+
+export function adventMain(input: string): any {
+    const lines = input.split('\n');
+    lines.forEach((line: string) => {
+
+    });
+
+    const g: Grid<string> = Grid.fromString<string>(input, ' ');
+    g.print(' ');
+
+    let count = 0;
+    g.data.forEach((row: string[]) => {
+        const nums = row.map((s) => parseInt(s));
+        let sameDir = true;
+        let dir = nums[1] - nums[0];
+        p(nums, dir);
+        let changeBad = false;
+        for (let i=1; i < nums.length; i++) {
+            let second = nums[i];
+            let first = nums[i-1];
+            p(first, second);
+            let diff = second - first;
+            p('diff', diff);
+            if(diff === 0 || Math.abs(diff) > 3) {
+                changeBad = true;
+                break;
+            }
+            p('dir:', diff/dir);
+            if(diff/dir < 0) {
+                sameDir = false;
+                break;
+            }
+        }
+        p(sameDir, changeBad);
+        if(sameDir && !changeBad) {
+            p('Safe!')
+            count++;
+        }
+        p();
+    });
+    return count;
+}
