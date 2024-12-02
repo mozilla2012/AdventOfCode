31f55b94cbe0c8d5f85c93875d429d9e51f9e75a
mozilla2012 <earnold1993@gmail.com>
2024-12-01 21:44:47 -0800

    2024 02 Part 2

--- 2024/days/02/02.ts
+++ 2024/days/02/02.ts
@@ -3,6 +3,7 @@
 import { Grid } from "../../util/Grid";
 import { p } from "../../util/utils";
 
+// Part 2
 export function adventMain(input: string): any {
     const lines = input.split('\n');
     lines.forEach((line: string) => {
@@ -15,28 +16,14 @@ export function adventMain(input: string): any {
     let count = 0;
     g.data.forEach((row: string[]) => {
         const nums = row.map((s) => parseInt(s));
-        let sameDir = true;
-        let dir = nums[1] - nums[0];
-        p(nums, dir);
-        let changeBad = false;
-        for (let i=1; i < nums.length; i++) {
-            let second = nums[i];
-            let first = nums[i-1];
-            p(first, second);
-            let diff = second - first;
-            p('diff', diff);
-            if(diff === 0 || Math.abs(diff) > 3) {
-                changeBad = true;
-                break;
-            }
-            p('dir:', diff/dir);
-            if(diff/dir < 0) {
-                sameDir = false;
-                break;
-            }
-        }
-        p(sameDir, changeBad);
-        if(sameDir && !changeBad) {
+        
+        const success = testLevel(nums, false) || nums.some((element: number, index) => {
+            const numsCopy = [...nums];
+            numsCopy.splice(index, 1);
+            return testLevel(numsCopy, false);
+        });
+       
+        if(success) {
             p('Safe!')
             count++;
         }
@@ -44,3 +31,71 @@ export function adventMain(input: string): any {
     });
     return count;
 }
+
+function testLevel(nums: number[], canRemove: boolean): boolean {
+    let sameDir = true;
+    let dir = nums[1] - nums[0];
+    p('testing...', nums, dir);
+    let changeBad = false;
+    for (let i=1; i < nums.length; i++) {
+        let second = nums[i];
+        let first = nums[i-1];
+        p(first, second);
+        let diff = second - first;
+        p('diff', diff);
+        if(diff === 0 || Math.abs(diff) > 3) {
+            changeBad = true;
+        }
+        p('dir:', diff/dir);
+        if(diff/dir < 0) {
+            sameDir = false;
+        }
+        if (!sameDir || changeBad) {
+            return false;
+        } 
+    }
+    return (true);
+}
+
+// Part 1
+// export function adventMain(input: string): any {
+//     const lines = input.split('\n');
+//     lines.forEach((line: string) => {
+
+//     });
+
+//     const g: Grid<string> = Grid.fromString<string>(input, ' ');
+//     g.print(' ');
+
+//     let count = 0;
+//     g.data.forEach((row: string[]) => {
+//         const nums = row.map((s) => parseInt(s));
+//         let sameDir = true;
+//         let dir = nums[1] - nums[0];
+//         p(nums, dir);
+//         let changeBad = false;
+//         for (let i=1; i < nums.length; i++) {
+//             let second = nums[i];
+//             let first = nums[i-1];
+//             p(first, second);
+//             let diff = second - first;
+//             p('diff', diff);
+//             if(diff === 0 || Math.abs(diff) > 3) {
+//                 changeBad = true;
+//                 break;
+//             }
+//             p('dir:', diff/dir);
+//             if(diff/dir < 0) {
+//                 sameDir = false;
+//                 break;
+//             }
+//         }
+//         p(sameDir, changeBad);
+//         if(sameDir && !changeBad) {
+//             p('Safe!')
+//             count++;
+//         }
+//         p();
+//     });
+//     return count;
+// }
