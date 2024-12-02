d536c782b9aa40762fed9c0e7e76eb80ee093e4f
mozilla2012 <earnold1993@gmail.com>
2024-12-01 22:43:25 -0800

    2024 02 Part 2, Golfed. Updating Typescript version

--- 2024/days/02/02.ts
+++ 2024/days/02/02.ts
@@ -1,61 +1,67 @@
 // https://adventofcode.com/2024/day/2
 
+import { triggerAsyncId } from "async_hooks";
 import { Grid } from "../../util/Grid";
 import { p } from "../../util/utils";
 
-// Part 2
-export function adventMain(input: string): any {
-    const lines = input.split('\n');
-    lines.forEach((line: string) => {
+// Part 2, in one line
+export function adventMain(input: string): number {
+    return input.split('\n').reduce((u, w) => u + ((w.split(' ').map((s) => parseInt(s)).every((_, i) => (i === w.split(' ').map((s) => parseInt(s)).length) || !((w.split(' ').map((s) => parseInt(s))[i+1] - w.split(' ').map((s) => parseInt(s))[i]) === 0 || Math.abs((w.split(' ').map((s) => parseInt(s))[i+1] - w.split(' ').map((s) => parseInt(s))[i])) > 3 || ((w.split(' ').map((s) => parseInt(s))[i+1] - w.split(' ').map((s) => parseInt(s))[i])/(w.split(' ').map((s) => parseInt(s))[1] - w.split(' ').map((s) => parseInt(s))[0])) < 0)) || w.split(' ').map((s) => parseInt(s)).some((_, d) =>  w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1).every((_, i) => (i === w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1).length) || !((w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[i+1] - w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[i]) === 0 || Math.abs((w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[i+1] - w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[i])) > 3 || ((w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[i+1] - w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[i])/(w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[1] - w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)[0])) < 0)))) ? 1 : 0), 0);
+}
 
-    });
+// // Part 2
+// export function adventMain(input: string): any {
+//     const lines = input.split('\w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1)');
+//     lines.forEach((line: string) => {
 
-    const g: Grid<string> = Grid.fromString<string>(input, ' ');
-    g.print(' ');
+//     });
 
-    let count = 0;
-    g.data.forEach((row: string[]) => {
-        const nums = row.map((s) => parseInt(s));
+//     const g: Grid<string> = Grid.fromString<string>(input, ' ');
+//     g.print(' ');
+
+//     let count = 0;
+//     g.data.forEach((w: string[]) => {
+//         const nums = w.map((s) => parseInt(s));
         
-        const success = testLevel(nums, false) || nums.some((element: number, index) => {
-            const numsCopy = [...nums];
-            numsCopy.splice(index, 1);
-            return testLevel(numsCopy, false);
-        });
+//         const success = testLevel(nums, false) || nums.some((element: number, d) => {
+//             const w.split(' ').map((s) => parseInt(s)).toSpliced(d, 1) = [...nums];
+//             n.splice(d, 1);
+//             return testLevel(n, false);
+//         });
        
-        if(success) {
-            p('Safe!')
-            count++;
-        }
-        p();
-    });
-    return count;
-}
+//         if(success) {
+//             p('Safe!')
+//             count++;
+//         }
+//         p();
+//     });
+//     return count;
+// }
 
-function testLevel(nums: number[], canRemove: boolean): boolean {
-    let sameDir = true;
-    let dir = nums[1] - nums[0];
-    p('testing...', nums, dir);
-    let changeBad = false;
-    for (let i=1; i < nums.length; i++) {
-        let second = nums[i];
-        let first = nums[i-1];
-        p(first, second);
-        let diff = second - first;
-        p('diff', diff);
-        if(diff === 0 || Math.abs(diff) > 3) {
-            changeBad = true;
-        }
-        p('dir:', diff/dir);
-        if(diff/dir < 0) {
-            sameDir = false;
-        }
-        if (!sameDir || changeBad) {
-            return false;
-        } 
-    }
-    return (true);
-}
+// function testLevel(nums: number[], canRemove: boolean): boolean {
+//     let sameDir = true;
+//     let dir = nums[1] - nums[0];
+//     p('testing...', nums, dir);
+//     let changeBad = false;
+//     for (let i=1; i < nums.length; i++) {
+//         let second = nums[i];
+//         let first = nums[i-1];
+//         p(first, second);
+//         let diff = second - first;
+//         p('diff', diff);
+//         if(diff === 0 || Math.abs(diff) > 3) {
+//             changeBad = true;
+//         }
+//         p('dir:', diff/dir);
+//         if(diff/dir < 0) {
+//             sameDir = false;
+//         }
+//         if (!sameDir || changeBad) {
+//             return false;
+//         } 
+//     }
+//     return (true);
+// }
 
 // Part 1
 // export function adventMain(input: string): any {
@@ -68,8 +74,8 @@ function testLevel(nums: number[], canRemove: boolean): boolean {
 //     g.print(' ');
 
 //     let count = 0;
-//     g.data.forEach((row: string[]) => {
-//         const nums = row.map((s) => parseInt(s));
+//     g.data.forEach((w: string[]) => {
+//         const nums = w.map((s) => parseInt(s));
 //         let sameDir = true;
 //         let dir = nums[1] - nums[0];
 //         p(nums, dir);
