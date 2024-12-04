77268a96bc691db920c0c3a9cc34c8e91d27f439
mozilla2012 <earnold1993@gmail.com>
2024-12-03 22:01:28 -0800

    2024 03 Part 2, slightly cleaned

--- 2024/days/03/03.ts
+++ 2024/days/03/03.ts
@@ -1,31 +1,22 @@
 // https://adventofcode.com/2024/day/3
 
-import { Grid } from "../../util/Grid";
-import { p } from "../../util/utils";
-
 // Part 2
 export function adventMain(input: string): any {
     const lines = input.split('\n');
     let sum = 0;
     let enabled = true;
     lines.forEach((line: string) => {
-
-        const regexp = /((mul\((\d{1}|\d{2}|\d{3}),(\d{1}|\d{2}|\d{3})\))|(don't\(\))|(do\(\)))/g;
+        const regexp = /((mul\((\d{1,3}),(\d{1,3})\))|(don't\(\))|(do\(\)))/g;
         const matches = line.matchAll(regexp);
-        p(matches);
-
-
         for (const match of matches) {
-            p(match)
             if(match[0] === 'don\'t()') {
                 enabled = false;
             } else if (match[0] === 'do()') {
                 enabled = true;
             } else if (enabled) {
-                sum += Number(match[3])* Number(match[4]);
+                sum += Number(match[3]) * Number(match[4]);
             }
         }
-
     });
     return sum;
 }
