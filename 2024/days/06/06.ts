80a33fe8fa58b859d8a26a05fb4c3cc7a5740816
mozilla2012 <earnold1993@gmail.com>
2024-12-08 18:14:22 -0800

    2024 06 Part 2

--- 2024/days/06/06.ts
+++ 2024/days/06/06.ts
@@ -1,19 +1,21 @@
 // https://adventofcode.com/2024/day/6
 
 import { Grid } from "../../util/Grid";
-const UP = 1;
-const RIGHT = 2;
-const DOWN = 3;
-const LEFT = 4;
+import { p } from "../../util/utils";
+const UP = 2;
+const RIGHT = 3;
+const DOWN = 5;
+const LEFT = 7;
 
 export function adventMain(input: string): any {    
     let dir = UP;
     const g: Grid<string> = Grid.fromString<string>(input);
+    const startingGrid: Grid<string> = Grid.fromString<string>(input);
     let pos: [number, number] = findStart(g); // row, col
-    let r = pos[0];
-    let c = pos[1];
-    g.data[r][c] = 'X';
-    let visits = 1;
+    let startR = pos[0];
+    let startC = pos[1];
+    g.data[startR][startC] = String(dir);
+    let loops = 0;
     while(true) {
         let r = pos[0];
         let c = pos[1];
@@ -23,16 +25,29 @@ export function adventMain(input: string): any {
                     const nextCell: string = g.getCell(r-1,c);
                     if(nextCell !== '#') {
                         pos = [r-1,c];
-                        if(nextCell === '.') {
-                            g.data[r-1][c] = 'X';
-                            visits++;
+                        if (nextCell === '.') {
+                            const g2 = Grid.copyGrid(startingGrid);
+                            g2.data[r-1][c] = '#';
+                            if(detectLoop(g2, startR, startC)) {
+                                // p('We have a loop!', r-1, c);
+                                loops++;
+                            }
+                            g.data[r-1][c] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                g.data[r-1][c] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
                         }
                     }
                     else {
                         dir = rotateDir(dir);
                     }
                 } catch (e) {
-                    return visits;
+                    p(g);
+                    return loops;
                 }
                 break; 
             } 
@@ -41,16 +56,29 @@ export function adventMain(input: string): any {
                     const nextCell: string = g.getCell(r,c+1);
                     if(nextCell !== '#') {
                         pos = [r,c+1];
-                        if(nextCell === '.') {
-                            g.data[r][c+1] = 'X';
-                            visits++;
+                        if (nextCell === '.') {
+                            const g2 = Grid.copyGrid(startingGrid);
+                            g2.data[r][c+1] = '#';
+                            if(detectLoop(g2, startR, startC)) {
+                                // p('We have a loop!', r-1, c);
+                                loops++;
+                            }
+                            g.data[r][c+1] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                g.data[r][c+1] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
                         }
                     }
                     else {
                         dir = rotateDir(dir);
                     }
                 } catch (e) {
-                    return visits;
+                    p(g);
+                    return loops;
                 }
                 break; 
             } 
@@ -59,16 +87,29 @@ export function adventMain(input: string): any {
                     const nextCell: string = g.getCell(r+1,c);
                     if(nextCell !== '#') {
                         pos = [r+1,c];
-                        if(nextCell === '.') {
-                            g.data[r+1][c] = 'X';
-                            visits++;
+                        if (nextCell === '.') {
+                            const g2 = Grid.copyGrid(startingGrid);
+                            g2.data[r+1][c] = '#';
+                            if(detectLoop(g2, startR, startC)) {
+                                // p('We have a loop!', r-1, c);
+                                loops++;
+                            }
+                            g.data[r+1][c] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                g.data[r+1][c] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
                         }
                     }
                     else {
                         dir = rotateDir(dir);
                     }
                 } catch (e) {
-                    return visits;
+                    p(g);
+                    return loops;
                 }
                 break; 
             } 
@@ -77,16 +118,29 @@ export function adventMain(input: string): any {
                     const nextCell: string = g.getCell(r,c-1);
                     if(nextCell !== '#') {
                         pos = [r,c-1];
-                        if(nextCell === '.') {
-                            g.data[r][c-1] = 'X';
-                            visits++;
+                        if (nextCell === '.') {
+                            const g2 = Grid.copyGrid(startingGrid);
+                            g2.data[r][c-1] = '#';
+                            if(detectLoop(g2, startR, startC)) {
+                                // p('We have a loop!', r-1, c);
+                                loops++;
+                            }
+                            g.data[r][c-1] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                g.data[r][c-1] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
                         }
                     }
                     else {
                         dir = rotateDir(dir);
                     }
                 } catch (e) {
-                    return visits;
+                    p(g);
+                    return loops;
                 }
                 break; 
             } 
@@ -95,7 +149,146 @@ export function adventMain(input: string): any {
             } 
         } 
     }
-    return visits;
+    return loops;
+}
+
+function detectLoop(g: Grid<string>, startRow: number, startCol:number): boolean {
+    let dir = UP;
+    let pos: [number, number] = [startRow, startCol];
+    let r = pos[0];
+    let c = pos[1];
+    g.data[r][c] = String(dir);
+
+    while(true) {
+        let r = pos[0];
+        let c = pos[1];
+        switch(dir) { 
+            case UP: {
+                try {
+                    const nextCell: string = g.getCell(r-1,c);
+                    if(nextCell !== '#') {
+                        pos = [r-1,c];
+                        if (nextCell === '.') {
+                            g.data[r-1][c] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                if(val % dir === 0) {
+                                    // p(val, dir, val % dir);
+                                    // g.data[r-1][c] = '%';
+                                    // // p(g);
+                                    return true;
+                                }
+                                g.data[r-1][c] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
+                        }
+                    }
+                    else {
+                        dir = rotateDir(dir);
+                    }
+                } catch (e) {
+                    return false;
+                }
+                break; 
+            } 
+            case RIGHT: { 
+                try {
+                    const nextCell: string = g.getCell(r,c+1);
+                    if(nextCell !== '#') {
+                        pos = [r,c+1];
+                        if (nextCell === '.') {
+                            g.data[r][c+1] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                if(val % dir === 0) {
+                                    // p(val, dir, val % dir);
+                                    // g.data[r][c+1] = '%';
+                                    // p(g);
+                                    return true;
+                                }
+                                g.data[r][c+1] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
+                        }
+                    }
+                    else {
+                        dir = rotateDir(dir);
+                    }
+                } catch (e) {
+                    return false;
+                }
+                break; 
+            } 
+            case DOWN: { 
+                try {
+                    const nextCell: string = g.getCell(r+1,c);
+                    if(nextCell !== '#') {
+                        pos = [r+1,c];
+                        if (nextCell === '.') {
+                            g.data[r+1][c] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                if(val % dir === 0) {
+                                    // p(val, dir, val % dir)
+                                    // g.data[r+1][c] = '%';
+                                    // p(g);
+                                    return true;
+                                }
+                                g.data[r+1][c] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
+                        }
+                    }
+                    else {
+                        dir = rotateDir(dir);
+                    }
+                } catch (e) {
+                    return false;
+                }
+                break; 
+            } 
+            case LEFT: { 
+                try {
+                    const nextCell: string = g.getCell(r,c-1);
+                    if(nextCell !== '#') {
+                        pos = [r,c-1];
+                        if (nextCell === '.') {
+                            g.data[r][c-1] = String(dir);
+                        } else {
+                            const val: number = parseInt(nextCell);
+                            if (val) {
+                                if(val % dir === 0) {
+                                    // p(val, dir, val % dir);
+                                    // g.data[r][c-1] = '%';
+                                    // p(g);
+                                    return true;
+                                }
+                                g.data[r][c-1] = String(val*dir);
+                            } else {
+                                p(`What is the next cell? ${nextCell}`);
+                            }
+                        }
+                    }
+                    else {
+                        dir = rotateDir(dir);
+                    }
+                } catch (e) {
+                    return false;
+                }
+                break; 
+            } 
+            default: { 
+                throw new Error('How?');
+            } 
+        } 
+    }
+    throw new Error('How did we get here?');
 }
 
 function findStart(g: Grid<string>): [number, number] {
