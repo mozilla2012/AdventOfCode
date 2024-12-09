33dce10937dafaea91b68ac3ff04019b691890cf
mozilla2012 <earnold1993@gmail.com>
2024-12-09 00:42:15 -0800

    2024 09 Part 2, cleaned up

--- 2024/days/09/09.ts
+++ 2024/days/09/09.ts
@@ -1,67 +1,27 @@
 // https://adventofcode.com/2024/day/9
 
+// Part 2
 export function adventMain(input: string): any {
-    const lines = input.split('\n');
+    const vals: number[] = input.split('\n')[0].split('').map(s => parseInt(s));
     let memory: number[] = [];
-    const vals: number[] = lines[0].split('').map(s=>parseInt(s));
-    const numFiles = Math.ceil(vals.length/2);
     let fileId = 0;
-    let reverseFileId = numFiles - 1;
-    let isFile = true;
-    let valIter = 0;
-    let fileSize = vals[valIter++];
-    let reverseFileSize = vals[reverseFileId*2];
-    let run = true;
-    let blankSize = 0;
-    while(run) {
-        if(isFile) {
-            fileSize--;
-            memory.push(fileId);
-            if(fileSize <= 0) {
-                isFile = false;
-                blankSize = vals[valIter++];
-                fileId++;
-                if(fileId >= numFiles) {
-                    run = false;
-                }
-            }
-        } else {
-            // Is blank:
-            if(blankSize <= 0) {
-                isFile = true;
-                fileSize = vals[valIter++];
-            } else {
-                memory.push(-1);
-                blankSize--;
-            }
-        }
-    }
-
+    // First, fill out the memory:
+    vals.forEach((itemSize, index) => memory.push(...Array(itemSize).fill((index % 2 === 0) ? fileId++ : -1)));
     // Then, move everything that you can:
-    reverseFileId = numFiles;
-    valIter = 0;
-    fileSize = vals[valIter++];
+    let reverseFileId = Math.ceil(vals.length / 2);
     while(reverseFileId-- > 0) {
-        const reverseFileIndex = memory.indexOf(reverseFileId);
-        reverseFileSize = vals[reverseFileId*2];        
-        // Find the first available gap that would work for this memory.
-        const slotStartIndex: number = findBigSlot(memory, reverseFileSize, reverseFileIndex);
+        const fileIndex = memory.indexOf(reverseFileId);
+        const fileSize = vals[reverseFileId * 2];        
+        // Find the first available gap that would work for this memory:
+        const slotStartIndex: number = memory.findIndex((_, i) => (memory[i] === -1 && i < fileIndex && memory.slice(i, i + fileSize).every((n: number) => n === -1)))
         if(slotStartIndex >= 0) {
-            memory.splice(slotStartIndex, reverseFileSize, ...memory.slice(reverseFileIndex, reverseFileIndex + reverseFileSize)); // Copy the memory to the new spot
-            memory.splice(reverseFileIndex, reverseFileSize, ...Array.from(Array(reverseFileSize)).map(() => -1)); // Copy blank memory to the old spot.
+            memory.splice(slotStartIndex, fileSize, ...memory.slice(fileIndex, fileIndex + fileSize)); // Copy the memory to the new spot
+            memory.splice(fileIndex, fileSize, ...Array(fileSize).fill(-1)); // Copy blank spaces to the old spot
         } // else, move on.
     }
-    return memory.reduce((checksum, val, i) => checksum + (((val === -1) ? 0 : val) * i), 0); // Calculate checksum
+    return memory.reduce((checksum, itemSize, index) => checksum + (((itemSize === -1) ? 0 : itemSize) * index), 0); // Calculate checksum
 }
 
-function findBigSlot(memory: number[], fileSize: number, endIndex: number): number {
-    for(let i = 0; i < endIndex; i++) {
-        if(memory[i] === -1 && memory.slice(i, i + fileSize).every((n: number) => n === -1)) {
-                return i;
-        }
-    }
-    return -1;
-}
 // Part 1
 // export function adventMain(input: string): any {
 //     const lines = input.split('\n');
