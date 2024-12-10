2dd1a190a253f33404a17b4e36e615e4b5747943
mozilla2012 <earnold1993@gmail.com>
2024-12-09 21:57:37 -0800

    2024 10 Part 2; cleaned up

--- 2024/days/10/10.ts
+++ 2024/days/10/10.ts
@@ -1,11 +1,9 @@
 // https://adventofcode.com/2024/day/10
 
 import { Grid } from "../../util/Grid";
-import { p } from "../../util/utils";
 
 export function adventMain(input: string): any {
     const g: Grid<number> = Grid.fromString<number>(input);
-    p(g);
     const startingPoints: [number, number][] = [];
     g.data.forEach((row: number[], rowI)=>{
         row.forEach((item: number, colI)=>{
@@ -14,20 +12,15 @@ export function adventMain(input: string): any {
             }
         });
     })
-    p(startingPoints);
     const scores: number[] = startingPoints.map((head: [number, number])=>{
-        // p('Starting with', head);
         let g2 = Grid.copyGrid(g);
-        let gV = Grid.fromDimensions<number>(g.numRows, g.numCols, 0);
         let val = 0;
         let numPaths = 1;
         let nextPath: [number, number][] = [head];
         while(val < 9 && numPaths > 0) {
-            let response = flood(g2, gV, val+1, nextPath);
-            // p(gV);
+            let response = flood(g2, val+1, nextPath);
             numPaths = response.numPaths;
             nextPath = response.nextPath;
-            // p(val, numPaths, nextPath);
             val++;
         }
         return numPaths;
@@ -35,16 +28,14 @@ export function adventMain(input: string): any {
     return scores.reduce((sum: number, item: number) => (sum+item), 0);
 }
 
-function flood(g: Grid<number>, gV: Grid<number>, targetVal: number, startingPoints: [number, number][]): {numPaths: number, nextPath: [number, number][]} {
+function flood(g: Grid<number>, targetVal: number, startingPoints: [number, number][]): {numPaths: number, nextPath: [number, number][]} {
     let numPaths = 0;
     let nextPath: [number, number][] = [];
     startingPoints.forEach((start: [number, number]) => {
-        // If any neighbor is the target val, mark it as 0.
         try { // Up:
             const nextVal = g.getCell(start[0] - 1, start[1]);
             if(nextVal === targetVal) {
                 numPaths++;
-                gV.data[start[0]-1][start[1]] = gV.data[start[0]-1][start[1]]+1;
                 nextPath.push([start[0] - 1, start[1]]);
             }
         } catch(e) {};
@@ -52,7 +43,6 @@ function flood(g: Grid<number>, gV: Grid<number>, targetVal: number, startingPoi
             const nextVal = g.getCell(start[0], start[1]+1);
             if(nextVal === targetVal) {
                 numPaths++;
-                gV.data[start[0]][start[1]+1] = gV.data[start[0]][start[1]+1]+1;
                 nextPath.push([start[0], start[1]+1]);
             }
         } catch(e) {};
@@ -60,7 +50,6 @@ function flood(g: Grid<number>, gV: Grid<number>, targetVal: number, startingPoi
             const nextVal = g.getCell(start[0] + 1, start[1]);
             if(nextVal === targetVal) {
                 numPaths++;
-                gV.data[start[0]+1][start[1]] = gV.data[start[0]+1][start[1]]+1;
                 nextPath.push([start[0]+1, start[1]]);
             }
         } catch(e) {};
@@ -68,7 +57,6 @@ function flood(g: Grid<number>, gV: Grid<number>, targetVal: number, startingPoi
             const nextVal = g.getCell(start[0], start[1]-1);
             if(nextVal === targetVal) {
                 numPaths++;
-                gV.data[start[0]][start[1]-1] = gV.data[start[0]][start[1]-1]+1;
                 nextPath.push([start[0], start[1]-1]);
             }
         } catch(e) {};
