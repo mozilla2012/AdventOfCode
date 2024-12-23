39053111ec7ce8fdea361718ea56b45421b631d2
mozilla2012 <earnold1993@gmail.com>
2024-12-22 21:28:00 -0500

    2024 22 Part 1, golfed

--- 2024/days/22/22.ts
+++ 2024/days/22/22.ts
@@ -1,39 +1,21 @@
 // https://adventofcode.com/2024/day/22
 
 export function adventMain(input: string): any {
-    const lines = input.split('\n');
-    let sum = 0;
-    lines.forEach((line: string) => {
-        let num = parseInt(line);
-        for(let i = 0; i< 2000; i++) {
-            num = s1(num);
-            num = s2(num);
-            num = s3(num);
-        }
-        sum += num;
-    });
-    return sum;
+    return input.split('\n').reduce((sum, line) => sum + Array.from(Array(2000)).reduce((liner) => s3(s2(s1(liner))), parseInt(line)), 0);
 }
 
 function s1(n: number): number {
-    const n1 = n * 64;
-    return prune(mix(n, n1));
+    return mixNPrune(n, n * 64);
 }
 
 function s2(n: number): number {
-    const n1 = Math.floor(n/32);
-    return prune(mix(n, n1));
+    return mixNPrune(n, Math.floor(n/32));
 }
 
 function s3(n: number): number {
-    const n1 = n * 2048;
-    return prune(mix(n, n1));
+    return mixNPrune(n, n * 2048);
 }
 
-function mix(s: number, n1: number): number {
-    return ((s ^ n1) >>> 0);
-}
-
-function prune(n: number): number {
-    return n % 16777216;
+function mixNPrune(s: number, n1: number): number {
+    return ((s ^ n1) >>> 0) % 16777216;
 }
